edfd01902ed1b916100abd89e32e17c91ba82c0d
javilesm <68437746+javilesm@users.noreply.github.com>
2023-04-13 10:10:54 -0600

    Update lemp.sh

--- lemp.sh
+++ lemp.sh
@@ -1,38 +1,38 @@
 #! /bin/bash
 # Instalar repositorios
 echo "Instalando repositorios..."
-yes '' | sudo add-apt-repository ppa:ondrej/php
-yes '' | sudo add-apt-repository ppa:ondrej/nginx
+yes '' | sudo add-apt-repository ppa:ondrej/php -qq
+yes '' | sudo add-apt-repository ppa:ondrej/nginx -qq
 sleep 5
 
 # Actualizar todos los paquete
 echo "Actualizando paquetes..."
-sudo apt update 
+sudo apt update -qq
 sleep 5
 
 
 # Instalar Tree
 echo "Instalando Tree.."
-sudo apt install tree -y 
+sudo apt install tree -yqq
 sleep 5
 
 # Instalar ZIP
 echo "Instalando ZIP..."
-sudo apt-get install zip -y
+sudo apt-get install zip -yqq
 sleep 5
 
 # Instalar NGINX
 echo "Instalando NGINX..."
-sudo apt install nginx -y
+sudo apt install nginx -yqq
 sleep 5
 
 # Instalar MySQL
 echo "Instalando MySQL..."
-sudo apt install mysql-server -y
+sudo apt install mysql-server -yqq
 sleep 5
 
 # Instalar PHP
 echo "Instalando PHP..."
-sudo apt install php-fpm php-cli php-common php-mysql php-mcrypt php-curl php-gd php-intl php-mbstring php-soap php-xml php-xmlrpc php-zip php-pear php-imagick php-imap php-tidy php-json php-bcmath php-apcu  -y
+sudo apt install php-fpm php-cli php-common php-mysql php-mcrypt php-curl php-gd php-intl php-mbstring php-soap php-xml php-xmlrpc php-zip php-pear php-imagick php-imap php-tidy php-json php-bcmath php-apcu  -yqq
 
 echo "Fin..."
