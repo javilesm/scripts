850e7c467e0ed44222e1e0ad9e56d27cde33c373
javilesm <68437746+javilesm@users.noreply.github.com>
2023-04-19 11:44:43 -0600

    Update aws_setup.sh

--- aws_setup.sh
+++ aws_setup.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 # Variables de configuración de AWS CLI
-aws_access_key="AKIAWMGWI7QEM3S7YNDN"
-aws_secret_access_key="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"
-aws_default_region="us-east-1"
+AWS_Access_Key="AKIAWMGWI7QEM3S7YNDN"
+AWS_Secret_Access_Key="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"
+AWS_Default_Region="us-east-1"
 
 # Comprobar si AWS CLI está instalado
 if ! command -v aws &> /dev/null; then
@@ -18,4 +18,18 @@ aws configure set aws_secret_access_key "$aws_secret_access_key"
 aws configure set default.region "$aws_default_region"
 aws configure set output.format json
 
+# Configurar AWS CLI
+aws configure set aws_access_key_id $AWS_Access_Key
+aws configure set aws_secret_access_key $AWS_Secret_Access_Key
+aws configure set default.region $AWS_Default_Region
+
+# Exportar variables de entorno
+export AWS_ACCESS_KEY_ID=$AWS_Access_Key
+export AWS_SECRET_ACCESS_KEY=$AWS_Secret_Access_Key
+export AWS_DEFAULT_REGION=$AWS_Default_Region
+
+# Verificar la configuración
+aws configure list
+
 echo "AWS CLI ha sido configurado exitosamente."
+echo "Las variables de entorno AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY y AWS_DEFAULT_REGION también han sido exportadas."
