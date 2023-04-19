be5069b8cc55acb9b75d1a835a2ebe43e1d9c6a4
javilesm <68437746+javilesm@users.noreply.github.com>
2023-04-19 16:22:33 -0600

    Update jq.sh

--- jq.sh
+++ jq.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-
+# SCRIPT PARA INSTALAR JQ
 # Verificar si se ejecuta el script como root
 if [[ $EUID -ne 0 ]]; then
    echo "Este script debe ser ejecutado como root" 
@@ -9,14 +9,11 @@ fi
 # Actualizar el sistema
 apt update
 
-# Instalar Curl
-apt install -y curl
-
 # Instalar JQ
 apt install -y jq
 
 # Imprimir mensaje de instalación exitosa
-echo "Curl y JQ han sido instalados exitosamente."
+echo "JQ ha sido instalado exitosamente."
 
 # Salir del script
 exit 0
