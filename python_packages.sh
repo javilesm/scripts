dae0997536f56f892e44b925d8c7adeb76eae787
javilesm <68437746+javilesm@users.noreply.github.com>
2023-04-20 20:38:27 -0600

    Update python_packages.sh

--- python_packages.sh
+++ python_packages.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
-# Python_Packages
+# python_packages.sh
 echo "***SCRIPT INSTALACION DE PAQUETES PYTHON***"
-
 instalar_paquetes() {
   while read paquete; do
     echo "Comenzando la instalación de $paquete..."
@@ -12,17 +11,14 @@ instalar_paquetes() {
     echo "$paquete se ha instalado correctamente."
   done < "$1"
 }
-
 # Obtenemos la ruta absoluta del directorio actual donde se encuentra el script y el archivo de paquetes
 DIRECTORIO_ACTUAL="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
-
 # Verificamos si el archivo "packages.txt" existe en el directorio actual
 PACKAGES_FILE="$DIRECTORIO_ACTUAL/packages.txt"
 if [ ! -f "$PACKAGES_FILE" ]; then
   echo "Error: el archivo de paquetes no se encontró en $DIRECTORIO_ACTUAL. Por favor, asegúrese de que el archivo se llame 'packages.txt' y esté en el directorio correcto."
   exit 1
 fi
-
 # Realizamos la instalación de manera recursiva
 echo "Comenzando la instalación de los paquetes de Python..."
 instalar_paquetes "$PACKAGES_FILE"
