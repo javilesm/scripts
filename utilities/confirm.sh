0c0ee7634306c853a94704d124e048bf62a8ba7e
javilesm <68437746+javilesm@users.noreply.github.com>
2023-04-22 20:36:29 -0600

    Create confirm.sh

---
+++ utilities/confirm.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# confirm.sh
+# Función para obtener la confirmación del usuario
+confirm() {
+  while true; do
+    read -p "$1 (y/n): " yn
+    case $yn in
+      [Yy]* ) return 0;;
+      [Nn]* ) return 1;;
+      * ) echo "Por favor, responda con 'y' o 'n'.";;
+    esac
+  done
+}
