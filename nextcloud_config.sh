66b929c82ee9faec604745c12506df56fce24e07
javilesm <68437746+javilesm@users.noreply.github.com>
2023-04-24 09:24:25 -0600

    Create nextcloud_config.sh

---
+++ nextcloud_config.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+# nextcloud_config.sh
+# Variables
+DOMAIN="localhost"
+# Configurar Nginx
+function configure_nginx() {
+  
+}
+# Configurar MySQL
+function configure_nginx() {
+
+}
+# Configurar PHP
+function configure_php() {
+
+}
+# Función principal
+function nextcloud_config() {
+  echo "**********NEXTCLOUD CONFIGURATOR***********"
+  configure_nginx
+  configure_nginx
+  configure_php
+  echo "**********ALL DONE***********"
+}
+# Llamar a la función principal
+nextcloud_config
