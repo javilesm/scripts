d373e54a9ec6a5048c38b603e4938faa4ea2524b
javilesm <68437746+javilesm@users.noreply.github.com>
2023-04-24 10:56:17 -0600

    Update lemp_install.sh

--- lemp_install.sh
+++ lemp_install.sh
@@ -74,7 +74,7 @@ function install_mysql () {
 function install_php () {
   if [ $(dpkg-query -W -f='${Status}' php 2>/dev/null | grep -c "ok installed") -eq 0 ]; then
     echo "Instalando PHP..."
-    if ! sudo apt install php-fpm php-cli php-common php-mysql php-mcrypt php-curl php-gd php-intl php-mbstring php-soap php-xml php-xmlrpc php-zip php-pear php-imagick php-imap php-tidy php-json php-bcmath php-apcu -yqq; then
+    if ! sudo apt install php-fpm php-cli php-common php-mysql php-mcrypt php-curl php-gd php-intl php-mbstring php-soap php-xml php-xmlrpc php-zip php-pear php-imagick php-imap php-ldap php-tidy php-json php-bcmath php-apcu -yqq; then
       echo "No se pudo instalar PHP"
       exit 1
     fi
