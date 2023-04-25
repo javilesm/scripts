63e28ab2a77c8de450c981a0e4404b5c5a782252
javilesm <68437746+javilesm@users.noreply.github.com>
2023-04-25 14:06:28 -0600

    Update jq_install.sh

--- JQ/jq_install.sh
+++ JQ/jq_install.sh
@@ -8,11 +8,6 @@ function check_root() {
      exit 1
   fi
 }
-# Función para actualizar el sistema
-function update_system() {
-  echo "Actualizando el sistema..."
-  apt update || { echo "Error al actualizar el sistema. Saliendo..." ; exit 1; }
-}
 # Función para instalar JQ
 function install_jq() {
   echo "Instalando JQ..."
@@ -22,7 +17,6 @@ function install_jq() {
 function jq_install () {
   echo "**********JQ INSTALL**********"
   check_root
-  update_system
   install_jq
   echo "**********ALL DONE**********"
 }
