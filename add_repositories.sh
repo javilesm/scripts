e0bc44cc4cf99840ea28cce8f37c286e163a8a5f
javilesm <68437746+javilesm@users.noreply.github.com>
2023-04-25 13:49:36 -0600

    Create add_repositories.sh

---
+++ add_repositories.sh
@@ -0,0 +1,19 @@
+#! /bin/bash
+# add_repositories.sh
+# Variables
+REPO_FILE="repositories.txt"
+CURRENT_PATH="$( cd "$( dirname "${0}" )" && pwd )" # Obtener el directorio actual
+REPO_PATH="$CURRENT_PATH/$REPO_FILE"
+
+# Función principal
+function add_repositories() {
+    echo "Agregando repositorios..."
+    while read -r repository; do
+        if ! yes '' | sudo add-apt-repository "$repository" -qq; then
+        echo "No se pudo agregar el repositorio: $repository"
+        exit 1
+        fi
+    done < "$REPO_PATH"
+}
+# Lllamar a la función principal
+add_repositories
