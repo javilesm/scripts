6e1dc0bf3f12832d37055a61118229ceeffe8b30
javilesm <68437746+javilesm@users.noreply.github.com>
2023-04-24 22:12:56 -0600

    Update postgresql_config.sh

--- postgresql_config.sh
+++ postgresql_config.sh
@@ -12,22 +12,6 @@ function check_root() {
         exit 1
     fi
 }
-# Función para crear un usuario en PostgreSQL
-function create_user() {
-    echo "Creando usuarios en PostgreSQL desde $USERS_PATH ..."
-    # Leer la lista de usuarios y contraseñas desde el archivo postgresql_users.csv
-    while IFS=, read -r username password; do
-        # Crear usuario
-        sudo -u postgres psql -c "CREATE USER $username WITH PASSWORD '$password';"
-
-        # Verificar que el usuario se ha creado correctamente
-        if ! sudo -u postgres psql -c "SELECT 1 FROM pg_roles WHERE rolname='$username'" | grep -q 1
-        then
-            echo "No se ha podido crear el usuario $username."
-            exit 1
-        fi
-    done < "$USERS_PATH"
-}
 # Función para crear una base de datos en PostgreSQL
 function create_db() {
     echo "Creando una base de datos en PostgreSQL..."
@@ -44,22 +28,47 @@ function create_db() {
         exit 1
     fi
 }
-# Función para otorgar permisos de acceso a un usuario en una base de datos de PostgreSQL
+# Función para crear un usuario en PostgreSQL
+function create_user() {
+    echo "Creando usuarios en PostgreSQL desde $USERS_PATH ..."
+    # Leer la lista de usuarios y contraseñas desde el archivo postgresql_users.csv
+    while IFS=, read -r username password; do
+        # Crear usuario
+        sudo -u postgres psql -c "CREATE USER $username WITH PASSWORD '$password';"
+
+        # Verificar que el usuario se ha creado correctamente
+        if ! sudo -u postgres psql -c "SELECT 1 FROM pg_roles WHERE rolname='$username'" | grep -q 1
+        then
+            echo "No se ha podido crear el usuario $username."
+            exit 1
+        fi
+    done < "$USERS_PATH"
+}
+
+# Función para otorgar permisos de acceso a un usuario en una o varias bases de datos de PostgreSQL
 function grant_access() {
-    echo "Otorgando permisos de acceso a un usuario en una base de datos de PostgreSQL..."
-    # Solicitar el nombre del usuario y la base de datos
-    read -p "Introduzca el nombre del usuario: " username
-    read -p "Introduzca el nombre de la base de datos: " dbname
+    echo "Otorgando permisos de acceso a un usuario en una o varias bases de datos de PostgreSQL..."
 
-    # Otorgar permisos de acceso
-    sudo -u postgres psql -c "GRANT ALL PRIVILEGES ON DATABASE $dbname TO $username;"
+    # Leer la información de los usuarios desde el archivo postgresql_users.csv
+    while IFS=',' read -r username password databases access_level; do
+        echo "Otorgando permisos de acceso para el usuario $username en las bases de datos $databases con nivel de acceso $access_level..."
 
-    # Verificar que se han otorgado los permisos correctamente
-    if ! sudo -u postgres psql -c "SELECT has_database_privilege('$username', '$dbname', 'CREATE');" | grep -q "t"
-    then
-        echo "No se han podido otorgar los permisos de acceso."
-        exit 1
-    fi
+        # Otorgar permisos de acceso a cada base de datos
+        IFS=';' read -ra dbs <<< "$databases"
+        for db in "${dbs[@]}"; do
+            sudo -u postgres psql -c "GRANT $access_level PRIVILEGES ON DATABASE $db TO $username;"
+        done
+
+        # Verificar que se han otorgado los permisos correctamente
+        for db in "${dbs[@]}"; do
+            if ! sudo -u postgres psql -c "SELECT has_database_privilege('$username', '$db', 'CREATE');" | grep -q "t"
+            then
+                echo "No se han podido otorgar los permisos de acceso al usuario $username en la base de datos $db."
+                exit 1
+            fi
+        done
+
+    done < "$SCRIPT_DIR/postgresql_users.csv"
 }
 # Función para reiniciar el servicio de PostgreSQL
 function restart_postgresql_service() {
@@ -70,8 +79,8 @@ function restart_postgresql_service() {
 function postgresql_config() {
     echo "**********POSTGRESQL CONFIG**********"
     check_root
-    create_user
     create_db
+    create_user
     grant_access
     restart_postgresql_service
     echo "**************ALL DONE**************"
