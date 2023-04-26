3e12a894f1d55392d0dae83bba3c20f2b37e7481
javilesm <68437746+javilesm@users.noreply.github.com>
2023-04-25 21:55:02 -0600

    Update mysql_config.sh

--- MySQL/mysql_config.sh
+++ MySQL/mysql_config.sh
@@ -2,7 +2,7 @@
 # mysql_config.sh
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 USERS_FILE="mysql_users.csv"
-USERS_PATH="$SCRIPT_DIR/$USER_FILE"
+USERS_PATH="$SCRIPT_DIR/$USERS_FILE"
 DBS_FILE="mysql_db.csv"
 DBS_PATH="$SCRIPT_DIR/$DBS_FILE"
 ROLES_FILE="mysql_roles.csv"
@@ -28,35 +28,37 @@ function start_service() {
 function check_user_file() {
     echo "Verificando la existencia del archivo de usuarios"
     if [ ! -f "$USERS_PATH" ]; then
-        echo "El archivo de usuarios $USERS_FILE no existe en el directorio $SCRIPT_DIR."
+        echo "El archivo de usuarios '$USERS_FILE' no existe en el directorio $SCRIPT_DIR/"
         exit 1
     fi
+    echo "El archivo de usuarios '$USERS_FILE' existe."
 }
 # Función para validar la existencia del archivo de bases de datos
 function check_dbs_file() {
     echo "Validando la existencia del archivo de bases de datos..."
     if [ ! -f "$DBS_PATH" ]; then
-        echo "El archivo de bases de datos $DBS_PATH no existe en el directorio $SCRIPT_DIR."
+        echo "El archivo de bases de datos '$DBS_FILE' no existe en el directorio $SCRIPT_DIR/"
         exit 1
     fi
+    echo "El archivo de bases de datos '$DBS_FILE' existe."
 }
 # Función para crear una base de datos en MySQL
 function create_db() {
-    echo "Creando bases de datos en MySQL desde $DBS_FILE ..."
+    echo "Creando bases de datos en MySQL desde '$DBS_FILE' ..."
     # Leer la lista de bases de datos desde el archivo mysql_databases.csv
     while IFS=, read -r dbname; do
         # Crear base de datos
         sudo mysql -e "CREATE DATABASE IF NOT EXISTS $dbname;"
-    done < "$DBS_FILE"
+    done < "$DBS_PATH"
 }
 # Función para crear un usuario en MySQL
 function create_user() {
-    echo "Creando usuarios en MySQL desde $USERS_FILE ..."
+    echo "Creando usuarios en MySQL desde '$USERS_FILE' ..."
     # Leer la lista de usuarios y contraseñas desde el archivo mysql_users.csv
     while IFS=, read -r username password address; do
         # Verificar si el usuario ya existe
         if sudo mysql -e "SELECT 1 FROM mysql.user WHERE user='$username'" | grep -q 1; then
-            echo "El usuario $username ya existe."
+            echo "El usuario '$username' ya existe."
             continue
         fi
 
@@ -66,10 +68,10 @@ function create_user() {
         # Verificar que el usuario se ha creado correctamente
         if ! sudo mysql -e "SELECT 1 FROM mysql.user WHERE user='$username'" | grep -q 1
         then
-            echo "No se ha podido crear el usuario $username."
+            echo "No se ha podido crear el usuario '$username'."
             exit 1
         fi
-    done < "$USER_FILE"
+    done < "$USERS_PATH"
 }
 
 # Función para otorgar permisos de acceso a un usuario en una o varias bases de datos de MySQL
@@ -81,17 +83,17 @@ function grant_access() {
 
         # Verificar si se pasó al menos una base de datos
         if [[ -z $databases ]]; then
-            echo "Se debe especificar al menos una base de datos para otorgar permisos de acceso al usuario $username desde $address."
+            echo "Se debe especificar al menos una base de datos para otorgar privilegios al usuario '$username' desde '$address'."
             exit 1
         fi
 
         # Verificar que los permisos tienen el formato correcto
         if ! [[ "$privileges" =~ ^(ALL PRIVILEGES|CREATE|ALTER|DROP|GRANT|SELECT|INSERT|UPDATE|DELETE)$ ]]; then
-            echo "El nivel de acceso $privileges no tiene un formato válido para el usuario $username en las bases de datos $databases desde $address."
+            echo "El privilegio '$privileges' no tiene un formato válido para el usuario '$username' en las bases de datos '$databases' desde '$address'."
             exit 1
         fi
 
-        echo "Otorgando permisos de acceso para el usuario $username en las bases de datos $databases con nivel de acceso $privileges..."
+        echo "Otorgando permisos de acceso para el usuario '$username' en las bases de datos '$databases' con nivel de acceso '$privileges'..."
 
         # Verificar si el usuario ya cuenta con los permisos
         existing_privileges=$(sudo mysql -e "SELECT privilege_type FROM mysql.db WHERE user='$username' AND db IN ('$databases') AND host='$address'")
@@ -111,7 +113,7 @@ function grant_access() {
         for db in "${databases//;/ }"; do
             if ! sudo mysql -e "SELECT 1 FROM mysql.db WHERE user='$username' AND db='$db' AND host='$address' AND privilege_type='$privileges'" | grep -q 1
             then
-                echo "No se han podido otorgar o actualizar los permisos de acceso al usuario $username en la base de datos $db desde $address."
+                echo "No se han podido otorgar o actualizar los permisos de acceso al usuario '$username' en la base de datos '$db' desde '$address'."
                 exit 1
             fi
         done
