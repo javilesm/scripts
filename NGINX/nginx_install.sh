362e25a1d5fb6ad15ac6e30f71cc0342a74dd2ee
javilesm <68437746+javilesm@users.noreply.github.com>
2023-04-28 19:50:46 -0600

    Update nginx_install.sh

--- NGINX/nginx_install.sh
+++ NGINX/nginx_install.sh
@@ -2,15 +2,52 @@
 # nginx_install.sh
 # Función para instalar NGINX
 function install_nginx () {
-  if [ $(dpkg-query -W -f='${Status}' nginx 2>/dev/null | grep -c "ok installed") -eq 0 ]; then
-    echo "Instalando NGINX..."
-    if ! sudo apt install nginx -yqq; then
-      echo "No se pudo instalar NGINX"
-      exit 1
+  install_and_restart nginx
+}
+# Función para instalar un paquete y reiniciar los servicios afectados
+function install_and_restart() {
+  local package="$1"
+  # Verificar si el paquete ya está instalado
+  echo "Verificando si el paquete ya está instalado..."
+  if dpkg -s "$package" >/dev/null 2>&1; then
+    echo "El paquete '$package' ya está instalado."
+    return 0
+  fi
+
+  # Instalar el paquete
+  echo "Instalando $package..."
+  if ! sudo apt-get install "$package" -y; then
+    echo "Error: no se pudo instalar el paquete '$package'."
+    return 1
+  fi
+  
+   # Verificar si el paquete se instaló correctamente
+   echo "Verificando si el paquete se instaló correctamente..."
+  if [ $? -eq 0 ]; then
+    echo "$package se ha instalado correctamente."
+  else
+    echo "Error al instalar $package."
+    return 1
+  fi
+  
+  # Buscar los servicios que necesitan reiniciarse
+  echo "Buscando los servicios que necesitan reiniciarse..."
+  services=$(systemctl list-dependencies --reverse "$package" | grep -oP '^\w+(?=.service)')
+
+  # Reiniciar los servicios que dependen del paquete instalado
+  echo "Reiniciando los servicios que dependen del paquete instalado..."
+  if [[ -n $services ]]; then
+    echo "Reiniciando los siguientes servicios: $services"
+    if ! sudo systemctl restart $services; then
+      echo "Error: no se pudieron reiniciar los servicios después de instalar el paquete '$package'."
+      return 1
     fi
   else
-    echo "NGINX ya está instalado."
+    echo "No se encontraron servicios que necesiten reiniciarse después de instalar el paquete '$package'."
   fi
+
+  echo "El paquete '$package' se instaló correctamente."
+  return 0
 }
 # Función principal
 function nginx_install() {
