623b65f55d534bab4e1c9f5e74fcba00498811c1
javilesm <68437746+javilesm@users.noreply.github.com>
2023-04-28 19:49:01 -0600

    Update jq_install.sh

--- JQ/jq_install.sh
+++ JQ/jq_install.sh
@@ -5,40 +5,51 @@
 function install_jq() {
   install_and_restart jq
 }
+
 # Función para instalar un paquete y reiniciar los servicios afectados
 function install_and_restart() {
   local package="$1"
-
   # Verificar si el paquete ya está instalado
+  echo "Verificando si el paquete ya está instalado..."
   if dpkg -s "$package" >/dev/null 2>&1; then
-    echo "$package ya está instalado."
-    return
+    echo "El paquete '$package' ya está instalado."
+    return 0
   fi
 
   # Instalar el paquete
   echo "Instalando $package..."
-  if sudo apt-get install "$package" -y >/dev/null 2>&1; then
-    echo "Instalación de $package completa."
-  else
-    echo "Error: no se pudo instalar $package."
+  if ! sudo apt-get install "$package" -y; then
+    echo "Error: no se pudo instalar el paquete '$package'."
     return 1
   fi
-
+  
+   # Verificar si el paquete se instaló correctamente
+   echo "Verificando si el paquete se instaló correctamente..."
+  if [ $? -eq 0 ]; then
+    echo "$package se ha instalado correctamente."
+  else
+    echo "Error al instalar $package."
+    return 1
+  fi
+  
   # Buscar los servicios que necesitan reiniciarse
+  echo "Buscando los servicios que necesitan reiniciarse..."
   services=$(systemctl list-dependencies --reverse "$package" | grep -oP '^\w+(?=.service)')
 
   # Reiniciar los servicios que dependen del paquete instalado
+  echo "Reiniciando los servicios que dependen del paquete instalado..."
   if [[ -n $services ]]; then
     echo "Reiniciando los siguientes servicios: $services"
-    if sudo systemctl restart $services >/dev/null 2>&1; then
-      echo "Reinicio de servicios completado."
-    else
-      echo "Error: no se pudieron reiniciar los siguientes servicios: $services."
+    if ! sudo systemctl restart $services; then
+      echo "Error: no se pudieron reiniciar los servicios después de instalar el paquete '$package'."
       return 1
     fi
   else
-    echo "No se encontraron servicios que necesiten reiniciarse."
+    echo "No se encontraron servicios que necesiten reiniciarse después de instalar el paquete '$package'."
   fi
+
+  echo "El paquete '$package' se instaló correctamente."
+  return 0
 }
 
 # Llamar a las funciones en el orden correcto
