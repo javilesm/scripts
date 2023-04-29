ac8796c5092748ae169d3bafdd42ccfd799a3aa4
javilesm <68437746+javilesm@users.noreply.github.com>
2023-04-28 19:57:11 -0600

    Update python_install.sh

--- Python/python_install.sh
+++ Python/python_install.sh
@@ -2,27 +2,55 @@
 # python_install.sh
 # Variables
 PARAMETERS_FILE="python_config.cfg" # Parámetros de configuración
-# Función para verificar si Python está instalado
-function verificar_python_instalado() {
-  echo "Verificando si Python está instalado..."
-  if ! command -v python3 >/dev/null 2>&1; then
-    echo "Python no está instalado"
-    return 1
-  fi
-  echo "Python está instalado:"
-  python3 --version
-  return 0
-}
 # Función para instalar Python
 function instalar_python() {
-  if verificar_python_instalado; then
-    echo "Python ya está instalado."
-    return
+  install_and_restart python3
+
+}
+# Función para instalar un paquete y reiniciar los servicios afectados
+function install_and_restart() {
+  local package="$1"
+  # Verificar si el paquete ya está instalado
+  echo "Verificando si el paquete ya está instalado..."
+  if dpkg -s "$package" >/dev/null 2>&1; then
+    echo "El paquete '$package' ya está instalado."
+    return 0
   fi
-  echo "Instalando Python..."
-  sudo apt-get update
-  sudo apt-get install -y python3
-  echo "Python ha sido instalado."
+
+  # Instalar el paquete
+  echo "Instalando $package..."
+  if ! sudo apt-get install "$package" -y; then
+    echo "Error: no se pudo instalar el paquete '$package'."
+    return 1
+  fi
+  
+   # Verificar si el paquete se instaló correctamente
+   echo "Verificando si el paquete se instaló correctamente..."
+  if [ $? -eq 0 ]; then
+    echo "$package se ha instalado correctamente."
+  else
+    echo "Error al instalar $package."
+    return 1
+  fi
+  
+  # Buscar los servicios que necesitan reiniciarse
+  echo "Buscando los servicios que necesitan reiniciarse..."
+  services=$(systemctl list-dependencies --reverse "$package" | grep -oP '^\w+(?=.service)')
+
+  # Reiniciar los servicios que dependen del paquete instalado
+  echo "Reiniciando los servicios que dependen del paquete instalado..."
+  if [[ -n $services ]]; then
+    echo "Reiniciando los siguientes servicios: $services"
+    if ! sudo systemctl restart $services; then
+      echo "Error: no se pudieron reiniciar los servicios después de instalar el paquete '$package'."
+      return 1
+    fi
+  else
+    echo "No se encontraron servicios que necesiten reiniciarse después de instalar el paquete '$package'."
+  fi
+
+  echo "El paquete '$package' se instaló correctamente."
+  return 0
 }
 # Función para obtener la versión de Python instalada en el sistema
 function obtener_version_python() {
@@ -76,7 +104,6 @@ function run_python_scripts() {
 # Función principal
 function python_install() {
   echo "*******PYTHON INSTALL*******"
-  verificar_python_instalado
   instalar_python
   obtener_version_python
   find_bashrc
