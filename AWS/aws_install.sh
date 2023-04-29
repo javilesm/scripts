f8f7de26e25acc5832e47c434cdc0e44d19f9cb2
javilesm <68437746+javilesm@users.noreply.github.com>
2023-04-28 19:48:13 -0600

    Update aws_install.sh

--- AWS/aws_install.sh
+++ AWS/aws_install.sh
@@ -4,59 +4,62 @@
 CURRENT_PATH="$( cd "$( dirname "${0}" )" && pwd )" # Obtener el directorio actual
 AWS_CONFIG="aws_config.sh" # Script configurador
 AWS_CONFIG_PATH="$CURRENT_PATH/$AWS_CONFIG"
-# Función para validar permisos de administrador
-function validate_admin_permissions() {
-  if [ "$(id -u)" != "0" ]; then
-  echo "Este script debe ser ejecutado como root o con permisos de sudo." 1>&2
-  exit 1
-  fi
-}
+
 # Función para instalar AWS CLI
 function install_aws_cli() {
   echo "Instalando AWS CLI."
   install_and_restart awscli
   echo "Instalación de AWS CLI completa."
 }
-#!/bin/bash
 # Función para instalar un paquete y reiniciar los servicios afectados
 function install_and_restart() {
   local package="$1"
-
   # Verificar si el paquete ya está instalado
+  echo "Verificando si el paquete ya está instalado..."
   if dpkg -s "$package" >/dev/null 2>&1; then
-    echo "$package ya está instalado."
-    return
+    echo "El paquete '$package' ya está instalado."
+    return 0
   fi
 
   # Instalar el paquete
   echo "Instalando $package..."
-  if sudo apt-get install "$package" -y >/dev/null 2>&1; then
-    echo "Instalación de $package completa."
-  else
-    echo "Error: no se pudo instalar $package."
+  if ! sudo apt-get install "$package" -y; then
+    echo "Error: no se pudo instalar el paquete '$package'."
     return 1
   fi
-
+  
+   # Verificar si el paquete se instaló correctamente
+   echo "Verificando si el paquete se instaló correctamente..."
+  if [ $? -eq 0 ]; then
+    echo "$package se ha instalado correctamente."
+  else
+    echo "Error al instalar $package."
+    return 1
+  fi
+  
   # Buscar los servicios que necesitan reiniciarse
+  echo "Buscando los servicios que necesitan reiniciarse..."
   services=$(systemctl list-dependencies --reverse "$package" | grep -oP '^\w+(?=.service)')
 
   # Reiniciar los servicios que dependen del paquete instalado
+  echo "Reiniciando los servicios que dependen del paquete instalado..."
   if [[ -n $services ]]; then
     echo "Reiniciando los siguientes servicios: $services"
-    if sudo systemctl restart $services >/dev/null 2>&1; then
-      echo "Reinicio de servicios completado."
-    else
-      echo "Error: no se pudieron reiniciar los siguientes servicios: $services."
+    if ! sudo systemctl restart $services; then
+      echo "Error: no se pudieron reiniciar los servicios después de instalar el paquete '$package'."
       return 1
     fi
   else
-    echo "No se encontraron servicios que necesiten reiniciarse."
+    echo "No se encontraron servicios que necesiten reiniciarse después de instalar el paquete '$package'."
   fi
+
+  echo "El paquete '$package' se instaló correctamente."
+  return 0
 }
 # Función para instalar S3FS
 function install_s3fs() {
   echo "Instalando S3FS."
-  sudo apt-get install s3fs -y || { echo "Ha ocurrido un error al instalar S3FS."; exit 1; }
+  install_and_restart s3fs
   echo "Instalación de S3FS completa."
 }
 # Función para verificar la existencia del archivo de configuración
@@ -75,8 +78,6 @@ function run_aws_cli_configurator() {
 # Función principal
 function aws_install () {
   echo "**********AWS INSTALL***********"
-  validate_admin_permissions
-  update_system
   install_aws_cli
   install_s3fs
   check_aws_config_file
