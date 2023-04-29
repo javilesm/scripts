069681b63ecd89cf8ac9b74b78ff442df1a3de03
javilesm <68437746+javilesm@users.noreply.github.com>
2023-04-28 19:45:27 -0600

    Update mysql_install.sh

--- MySQL/mysql_install.sh
+++ MySQL/mysql_install.sh
@@ -3,44 +3,56 @@
 # Variables
 CONFIG_FILE="mysql_config.sh"
 CURRENT_PATH="$( cd "$( dirname "${0}" )" && pwd )" # Obtener el directorio actual
-# Función para verificar si se ejecuta el script como root
-function check_root() {
-  echo "Verificando si se ejecuta el script como root"
-  if [[ $EUID -ne 0 ]]; then
-    echo "Este script debe ser ejecutado como root"
-    exit 1
-  fi
-}
-# Función para verificar si MySQL ya está instalado
-function check_mysql_installed() {
-  echo "Verificando si MySQL ya está instalado"
-  if command -v mysql &> /dev/null
-  then
-    echo "MySQL ya está instalado en este sistema."
-    exit 0
-  fi
-}
+
 # Función para instalar MySQL
 function install_mysql () {
-  if [ $(dpkg-query -W -f='${Status}' mysql-server 2>/dev/null | grep -c "ok installed") -eq 0 ]; then
-    echo "Instalando MySQL..."
-    if ! sudo apt-get install mysql-server -y; then
-      echo "No se pudo instalar MySQL"
-      exit 1
+  install_and_restart mysql-server
+}
+
+# Función para instalar un paquete y reiniciar los servicios afectados
+function install_and_restart() {
+  local package="$1"
+  # Verificar si el paquete ya está instalado
+  echo "Verificando si el paquete ya está instalado..."
+  if dpkg -s "$package" >/dev/null 2>&1; then
+    echo "El paquete '$package' ya está instalado."
+    return 0
+  fi
+
+  # Instalar el paquete
+  echo "Instalando $package..."
+  if ! sudo apt-get install "$package" -y; then
+    echo "Error: no se pudo instalar el paquete '$package'."
+    return 1
+  fi
+  
+   # Verificar si el paquete se instaló correctamente
+   echo "Verificando si el paquete se instaló correctamente..."
+  if [ $? -eq 0 ]; then
+    echo "$package se ha instalado correctamente."
+  else
+    echo "Error al instalar $package."
+    return 1
+  fi
+  
+  # Buscar los servicios que necesitan reiniciarse
+  echo "Buscando los servicios que necesitan reiniciarse..."
+  services=$(systemctl list-dependencies --reverse "$package" | grep -oP '^\w+(?=.service)')
+
+  # Reiniciar los servicios que dependen del paquete instalado
+  echo "Reiniciando los servicios que dependen del paquete instalado..."
+  if [[ -n $services ]]; then
+    echo "Reiniciando los siguientes servicios: $services"
+    if ! sudo systemctl restart $services; then
+      echo "Error: no se pudieron reiniciar los servicios después de instalar el paquete '$package'."
+      return 1
     fi
   else
-    echo "MySQL ya está instalado."
+    echo "No se encontraron servicios que necesiten reiniciarse después de instalar el paquete '$package'."
   fi
-}
-# Función para comprobar si MySQL se ha instalado correctamente
-function check_mysql_installation() {
-  echo "Comprobando si MySQL se ha instalado correctamente"
-  if ! command -v mysql &> /dev/null
-  then
-    echo "MySQL no se ha instalado correctamente."
-    exit 1
-  fi
-  echo "MySQL se ha instalado correctamente."
+
+  echo "El paquete '$package' se instaló correctamente."
+  return 0
 }
 # Función para verificar si el archivo de configuración de MySQL existe
 function check_mysql_config_file() {
@@ -93,8 +105,6 @@ function execute_config_file() {
 # Función principal
 function mysql_install() {
     echo "*******MYSQL INSTALL******"
-    check_root
-    check_mysql_installed
     install_mysql
     check_mysql_config_file
     backup_mysql_config_file
