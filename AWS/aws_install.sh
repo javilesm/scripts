aa5ad3ddda0c840a8156dd8920d4808ba28102e2
javilesm <68437746+javilesm@users.noreply.github.com>
2023-04-28 19:30:49 -0600

    Update aws_install.sh

--- AWS/aws_install.sh
+++ AWS/aws_install.sh
@@ -14,8 +14,44 @@ function validate_admin_permissions() {
 # Función para instalar AWS CLI
 function install_aws_cli() {
   echo "Instalando AWS CLI."
-  sudo apt-get install awscli -y || { echo "Ha ocurrido un error al instalar AWS CLI."; exit 1; }
+  install_and_restart awscli
   echo "Instalación de AWS CLI completa."
+}
+#!/bin/bash
+# Función para instalar un paquete y reiniciar los servicios afectados
+function install_and_restart() {
+  local package="$1"
+
+  # Verificar si el paquete ya está instalado
+  if dpkg -s "$package" >/dev/null 2>&1; then
+    echo "$package ya está instalado."
+    return
+  fi
+
+  # Instalar el paquete
+  echo "Instalando $package..."
+  if sudo apt-get install "$package" -y >/dev/null 2>&1; then
+    echo "Instalación de $package completa."
+  else
+    echo "Error: no se pudo instalar $package."
+    return 1
+  fi
+
+  # Buscar los servicios que necesitan reiniciarse
+  services=$(systemctl list-dependencies --reverse "$package" | grep -oP '^\w+(?=.service)')
+
+  # Reiniciar los servicios que dependen del paquete instalado
+  if [[ -n $services ]]; then
+    echo "Reiniciando los siguientes servicios: $services"
+    if sudo systemctl restart $services >/dev/null 2>&1; then
+      echo "Reinicio de servicios completado."
+    else
+      echo "Error: no se pudieron reiniciar los siguientes servicios: $services."
+      return 1
+    fi
+  else
+    echo "No se encontraron servicios que necesiten reiniciarse."
+  fi
 }
 # Función para instalar S3FS
 function install_s3fs() {
