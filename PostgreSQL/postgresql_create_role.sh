80ef0b2f1d6c8a638e4624e36704d721c75402da
javilesm <68437746+javilesm@users.noreply.github.com>
2023-05-01 07:12:20 -0600

    Update postgresql_create_role.sh

--- PostgreSQL/postgresql_create_role.sh
+++ PostgreSQL/postgresql_create_role.sh
@@ -4,7 +4,6 @@
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 ROLES_FILE="postgresql_roles.csv"
 ROLES_PATH="$SCRIPT_DIR/$ROLES_FILE"
-
 # Función para validar la existencia del archivo de roles
 function check_roles_file() {
     echo "Validando la existencia del archivo de roles..."
@@ -21,20 +20,27 @@ function create_roles() {
     # Leer la lista de roles de usuario desde el archivo postgresql_roles.csv
     while IFS=, read -r rolename password; do
         # Crear el rol de usuario
-        sudo -u postgres psql -c "CREATE ROLE $rolename LOGIN PASSWORD '$password';"
-
+        echo "Creando el rol '$rolename'..."
+        if ! sudo -u postgres psql -c "CREATE ROLE $rolename LOGIN PASSWORD '$password';"; then
+            echo "Error al crear el rol '$rolename'."
+            exit 1
+        fi
+        echo "El rol '$rolename' ha sido creado exitosamente."
         # Verificar que el rol de usuario se ha creado correctamente
+        echo "Verificando que el rol de usuario '$rolename' se haya creado correctamente..."
         if ! sudo -u postgres psql -c "SELECT 1 FROM pg_roles WHERE rolname='$rolename'" | grep -q 1; then
             echo "No se ha podido crear el rol de usuario '$rolename'."
             exit 1
         fi
+        echo "El rol '$rolename' ha sido verificado exitosamente."
     done < <(sed -e '$a\' "$ROLES_PATH")
+    echo "Todas los roles en '$ROLES_FILE' fueron creados."
 }
 # Función para mostrar todos los roles
 function show_roles() {
+    echo "Mostrando todos los roles en PostgreSQL..."
     sudo -u postgres psql -c "\du"
 }
-
 # Función principal
 function postgresql_create_role() {
     echo "**********POSTGRESQL CREATE ROLE**********"
