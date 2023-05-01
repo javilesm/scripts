1a16530abc66dd8fba3005514565229f04f4342d
javilesm <68437746+javilesm@users.noreply.github.com>
2023-04-30 21:42:39 -0600

    Update php_config.sh

--- PHP/php_config.sh
+++ PHP/php_config.sh
@@ -1,3 +1,36 @@
 #!/bin/bash
 # php_config.sh
 # Variables
+# Configurar PHP
+function configure_php() {
+  php_version=$(php -v | head -n 1 | cut -d " " -f 2 | cut -f1-2 -d".")
+  if [ -z "$php_version" ]; then
+    echo "Error: Unable to determine PHP version."
+    return 1
+  fi
+
+  echo "Configurando PHP version: $php_version..."
+  if sudo sed -i "s/memory_limit = .*/memory_limit = 512M/" /etc/php/$php_version/fpm/php.ini &&
+     sudo sed -i "s/upload_max_filesize = .*/upload_max_filesize = 100G/" /etc/php/$php_version/fpm/php.ini &&
+     sudo sed -i "s/post_max_size = .*/post_max_size = 100G/" /etc/php/$php_version/fpm/php.ini &&
+     sudo sed -i "s/;date.timezone.*/date.timezone = America\/Mexico_City/" /etc/php/$php_version/fpm/php.ini; then
+    echo "PHP configurado con éxito."
+  else
+    echo "Error al configurar PHP."
+    return 1
+  fi
+}
+
+# Reiniciar servicios
+function restart_services() {
+  sudo systemctl restart php$php_version-fpm
+}
+# Función principal
+function php_config() {
+  echo "**********PHP CONFIGURATOR***********"
+  configure_php
+  restart_services
+  echo "**********ALL DONE***********"
+}
+# Llamar a la función principal
+php_config
