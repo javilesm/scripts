3cc26ea3f749e2b97981188b1671dc065085c876
javilesm <68437746+javilesm@users.noreply.github.com>
2023-05-03 11:58:07 -0600

    Update nextcloud_install.sh

--- NEXTCLOUD/nextcloud_install.sh
+++ NEXTCLOUD/nextcloud_install.sh
@@ -4,80 +4,61 @@
 CURRENT_PATH="$( cd "$( dirname "${0}" )" && pwd )" # Obtener el directorio actual
 CONFIG_FILE="nextcloud_config.sh" # Script configurador
 CONFIG_PATH="$CURRENT_PATH/$CONFIG_FILE"
-HTML_PATH="/var/www/html/"
-NEXTCLOUD="nextcloud"
-# Función para verificar si el paquete ya está instalado
-function verify_nextcloud() {
-    # Verifica si el paquete de Nextcloud está instalado usando dpkg
-    if dpkg -s nextcloud &> /dev/null; then
-        echo "Nextcloud está instalado en este sistema."
-    else
-        echo "Nextcloud no está instalado en este sistema."
-    fi
-}
-# Función para descargar la última versión de Nextcloud
-function download_nextcloud() {
-    local version="26.0.1"
-    local url="https://download.nextcloud.com/server/releases/nextcloud-$version.zip"
-    echo "Descargando '$NEXTCLOUD' en su versión : $version..."
-    if ! sudo wget -q --show-progress "$url" -O "$NEXTCLOUD".zip; then
-        echo "Ha ocurrido un error al descargar $NEXTCLOUD-$version."
-        return 1
-    fi
-    echo "$NEXTCLOUD-$version se ha descargado con éxito."
-}
-# Función para desempaquetar el archivo descargado
-function unpack_nextcloud() {
-    echo "Desempaquetando el archivo descargado..."
-    if ! unzip -q "$NEXTCLOUD".zip; then
-        echo "Ha ocurrido un error al desempaquetar "$NEXTCLOUD".zip."
-        return 1
-    fi
-    echo "Verificando el directorio '$NEXTCLOUD'..."
-    if [[ ! -d "$NEXTCLOUD" ]]; then
-        echo "No se ha encontrado el directorio '$NEXTCLOUD' después de desempaquetar "$NEXTCLOUD".zip."
-        return 1
-    fi
-    echo "El archivo "$NEXTCLOUD".zip se ha desempaquetado correctamente en el directorio '$NEXTCLOUD'."
-}
-# Función para mover el directorio de Nextcloud a la raíz de NGINX
-function move_nextcloud() {
-    # Mover el directorio de Nextcloud a la raíz de NGINX
-    echo "Moviendo el directorio '$NEXTCLOUD' a la raíz de NGINX.."
-    sudo mv "$NEXTCLOUD" "$HTML_PATH" || { echo "Ha ocurrido un error al mover el directorio de Nextcloud."; exit 1; }
-    if [ ! -d "$HTML_PATH/$NEXTCLOUD" ]; then
-        echo "Ha ocurrido un error al mover el directorio de Nextcloud."
-        exit 1
-    fi
-    echo "El directorio '$NEXTCLOUD' se ha movido correctamente."
+HTML_PATH="/var/www/html"
+NEXTCLOUD_DIR="nextcloud"
+NEXTCLOUD_HTML_PATH="$HTML_PATH/$NEXTCLOUD_DIR"
+# Funcion para crear el directorio Nextcloud
+function mkdir_nextcloud() {
+  # Verificar si el directorio ya existe
+  if [ -d "$NEXTCLOUD_HTML_PATH" ]; then
+    echo "El directorio '$NEXTCLOUD_HTML_PATH' ya existe."
+    return
+  fi
+  # Creando el directorio Nextcloud
+  echo "Creando el directorio Nextcloud..."
+  if sudo mkdir -p "$NEXTCLOUD_HTML_PATH"; then
+    echo "El directorio '$NEXTCLOUD_HTML_PATH' se creó correctamente."
+  else
+    echo "Error al crear el directorio '$NEXTCLOUD_HTML_PATH'."
+  fi
 }
 # Función para darle al directorio de Nextcloud los permisos necesarios
 function set_nextcloud_permissions() {
-    echo "Dando al directorio '$NEXTCLOUD' los permisos necesarios..."
-    if sudo chown -R www-data:www-data "$HTML_PATH/$NEXTCLOUD"; then
-        echo "Los permisos se han establecido correctamente."
+    echo "Dando al directorio '$NEXTCLOUD_HTML_PATH' los permisos necesarios..."
+    if sudo chown -R www-data:www-data "$NEXTCLOUD_HTML_PATH"; then
+        echo "Los permisos se han establecido correctamente al directorio '$NEXTCLOUD_HTML_PATH'."
     else
-        echo "Ha ocurrido un error al establecer los permisos al directorio '$NEXTCLOUD'."
+        echo "Ha ocurrido un error al establecer los permisos al directorio '$NEXTCLOUD_HTML_PATH'."
         exit 1
     fi
 }
-# Función para iniciar Nextcloud como servicio
-function start_service() {
-    # Iniciar Nextcloud como servicio
-    echo "Iniciando Nextcloud como servicio..."
-    if ! sudo service snap.nextcloud.nginx start; then
-        echo "Error al iniciar Nextcloud como servicio."
-        return 1
+# Función para obtener Nextcloud usando wget
+function get_nextcloud() {
+  # Descargar Nextcloud con wget
+  echo "Descargando Nextcloud con wget en '$NEXTCLOUD_HTML_PATH'..."
+  if sudo wget "$NEXTCLOUD_HTML_PATH" "https://download.nextcloud.com/server/installer/setup-nextcloud.php"; then
+    echo "Nextcloud se descargó correctamente en '$NEXTCLOUD_HTML_PATH'."
+    cd "$NEXTCLOUD_HTML_PATH" & ll
+  else
+    echo "Error al descargar Nextcloud."
+    return
+  fi
+}
+# Función para darle al archivo de Nextcloud los permisos necesarios
+function set_setup_permissions() {
+    echo "Dando al archivo '$NEXTCLOUD_HTML_PATH/setup-nextcloud.php' los permisos necesarios..."
+    if sudo chown www-data:www-data "$NEXTCLOUD_HTML_PATH/setup-nextcloud.php"; then
+        echo "Los permisos se han establecido correctamente al archivo '$NEXTCLOUD_HTML_PATH/setup-nextcloud.php'."
+    else
+        echo "Ha ocurrido un error al establecer los permisos al archivo '$NEXTCLOUD_HTML_PATH/setup-nextcloud.php'."
+        exit 1
     fi
-
-    echo "Nextcloud se ha iniciado correctamente."
-    return 0
 }
 # Función para verificar si el archivo de configuración existe
 function validate_config_file() {
   echo "Verificando si el archivo de configuración existe..."
   if [ ! -f "$CONFIG_PATH" ]; then
-    echo "El archivo de configuración de Nextcloud no se puede encontrar."
+    echo "ERROR: El archivo de configuración de Nextcloud no se puede encontrar."
     exit 1
   fi
   echo "El archivo de configuración de Nextcloud existe."
@@ -89,22 +70,21 @@ function nextcloud_config() {
   if sudo bash "$CONFIG_PATH"; then
     echo "El archivo de configuración '$CONFIG_FILE' se ha ejecutado correctamente."
   else
-    echo "No se pudo ejecutar el archivo de configuración '$CONFIG_FILE'."
+    echo "ERROR: No se pudo ejecutar el archivo de configuración '$CONFIG_FILE'."
     exit 1
   fi
   echo "Configurador '$CONFIG_FILE' ejecutado."
 }
 # Función principal
-function main () {
+function nextcloud_install() {
   echo "**********NEXTCLOUD INSTALL***********"
-  download_nextcloud
-  unpack_nextcloud
-  move_nextcloud
-  set_nextcloud_permissions
-  start_service
-  validate_config_file
-  nextcloud_config
+    mkdir_nextcloud
+    set_nextcloud_permissions
+    get_nextcloud
+    set_setup_permissions
+    validate_config_file
+    nextcloud_config
   echo "*************ALL DONE**************"
 }
 # Llamar a la función principal
-main
+nextcloud_install
