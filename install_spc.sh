c7646ba349e76bd06885618bd741a7e585ad8a88
javilesm <68437746+javilesm@users.noreply.github.com>
2023-05-03 15:48:53 -0600

    Create install_spc.sh

---
+++ install_spc.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+# install_spc.sh
+# Funcion para instalar utilidades
+function instalar_utilidades() {
+  # instalar utilidades
+  echo "Instalando utilidades..."
+  if sudo apt-get install software-properties-common -y; then
+    echo "Utilidades instaladas."
+  else
+    echo "Error al instalar las utilidades."
+    exit 1
+  fi
+}
+# Funcion principal
+function install_spc() {
+  echo "**********INSTALL SOFTWARE-PROPERTIES-COMMON**********"
+  instalar_utilidades
+  echo "**********ALL DONE**********"
+}
+# Llamar a la funcion principal
+install_spc
