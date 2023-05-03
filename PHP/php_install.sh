aa2377edeca2960feb3c448e9a94f8c9b5a243cc
javilesm <68437746+javilesm@users.noreply.github.com>
2023-05-03 13:13:34 -0600

    Update php_install.sh

--- PHP/php_install.sh
+++ PHP/php_install.sh
@@ -6,23 +6,41 @@ PHP_MODULES_FILE="php_modules.txt"
 PHP_MODULES_PATH="$CURRENT_PATH/$PHP_MODULES_FILE" # Define la ruta del archivo de texto con los nombres de paquetes PHP
 CONFIG_FILE="php_config.sh"
 CONFIG_PATH="$CURRENT_PATH/$CONFIG_FILE"
-# Función para instalar PHP si no está instalado
-function install_php () {
+# Función para validar si PHP está instalado
+function validate_php() {
   if [ $(dpkg-query -W -f='${Status}' php 2>/dev/null | grep -c "ok installed") -eq 0 ]; then
-    echo "Instalando PHP..."
-    if ! sudo apt install $(cat "$PHP_MODULES_PATH") -yqq; then
-      echo "No se pudo instalar PHP"
-      exit 1
-    fi
+    echo "PHP no está instalado."
+    return 1
   else
     echo "PHP ya está instalado."
+    return 0
   fi
+}
+# Función para validar la existencia del archivo con la lista de módulos
+function validate_php_modules_file() {
+  echo "Validando la existencia de $PHP_MODULES_FILE..."
+  if [ ! -f "$PHP_MODULES_PATH" ]; then
+    echo "ERROR: no se encontró el archivo '$PHP_MODULES_FILE' en $CURRENT_PATH."
+    exit 1
+  fi
+  echo "$PHP_MODULES_FILE existe."
+}
+# Función para instalar los módulos PHP del archivo php_modules.txt
+function install_php_modules() {
+  echo "Instalando módulos PHP..."
+  while read module; do
+    if ! sudo apt install "$module" -yqq; then
+      echo "ERROR: No se pudo instalar el módulo $module"
+    else
+      echo "Módulo $module instalado correctamente."
+    fi
+  done < "$PHP_MODULES_PATH"
 }
 # Función para validar la existencia de php_config.sh
 function validate_config_file() {
   echo "Validando la existencia de $CONFIG_FILE..."
   if [ ! -f "$CONFIG_PATH" ]; then
-    echo "Error: no se encontró el archivo de configuración '$CONFIG_FILE' en $CURRENT_PATH."
+    echo "ERROR: no se encontró el archivo de configuración '$CONFIG_FILE' en $CURRENT_PATH."
     exit 1
   fi
   echo "$CONFIG_FILE existe."
@@ -34,7 +52,7 @@ function php_config() {
   if sudo bash "$CONFIG_PATH"; then
     echo "El archivo de configuración '$CONFIG_FILE' se ha ejecutado correctamente."
   else
-    echo "No se pudo ejecutar el archivo de configuración '$CONFIG_FILE'."
+    echo "ERROR: No se pudo ejecutar el archivo de configuración '$CONFIG_FILE'."
     exit 1
   fi
   echo "Configurador de PHP ejecutado."
@@ -42,7 +60,9 @@ function php_config() {
 # Función principal
 function php_install() {
     echo "*******PHP INSTALL******"
-    install_php
+    validate_php
+    validate_php_modules_file
+    install_php_modules
     validate_config_file
     php_config
     echo "*********ALL DONE********"
