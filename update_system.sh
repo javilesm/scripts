ce5322c4735b198bb9606f296042b30615d3d0ce
javilesm <68437746+javilesm@users.noreply.github.com>
2023-05-02 22:25:45 -0600

    Update update_system.sh

--- update_system.sh
+++ update_system.sh
@@ -2,8 +2,33 @@
 # update_system.sh
 # Funcion para actualizar sistema
 function actualizar_sistema() {
+  # actualizar sistema
   echo "Actualizando sistema..."
-  sudo apt-get update -y
+  if sudo apt-get update -y; then
+    echo "Sistema actualizado."
+  else
+    echo "Error al actualizar el sistema."
+    exit 1
+  fi
+}
+# Funcion para instalar utilidades
+function instalar_utilidades() {
+  # instalar utilidades
+  echo "Instalando utilidades..."
+  if sudo apt-get install software-properties-common -y; then
+    echo "Utilidades instaladas."
+  else
+    echo "Error al instalar las utilidades."
+    exit 1
+  fi
+}
+# Funcion principal
+function update_system() {
+  echo "**********UPDATE SYSTEM**********"
+  actualizar_sistema
+  instalar_utilidades
+  actualizar_sistema
+  echo "**********ALL DONE**********"
 }
 # Llamar a la funcion principal
-actualizar_sistema
+update_system
