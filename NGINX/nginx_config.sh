83dcb0e33e6d9e32098b58155338b92b60512cf3
javilesm <68437746+javilesm@users.noreply.github.com>
2023-05-02 21:56:17 -0600

    Update nginx_config.sh

--- NGINX/nginx_config.sh
+++ NGINX/nginx_config.sh
@@ -1,27 +1,28 @@
 #! /bin/bash
 # nginx_config.sh
 # Variables
-CURRENT_PATH="$( cd "$( dirname "${0}" )" && pwd )" # Obtener el directorio actual
 HTML_PATH="/var/www/html/"
 # Función para crear el directorio principal de Nginx
 function mkdir() {
   # Verificar si el directorio ya existe
   if [ -d "$HTML_PATH" ]; then
-    echo "El directorio principal de Nginx ya existe en la ruta especificada."
+    echo "El directorio principal de Nginx ya existe en la ruta especificada. Ruta: '$HTML_PATH'"
   else
     # crear el directorio principal de Nginx
     echo "Creando el directorio principal de Nginx..."
     if sudo mkdir -p "$HTML_PATH"; then
       echo "El directorio principal de NGINX se ha creado correctamente."
     else
-      echo "Error: No se pudo crear el directorio principal de Nginx en la ruta especificada."
+      echo "Error: No se pudo crear el directorio principal de Nginx en la ruta especificada. Ruta: '$HTML_PATH'"
       exit 1
     fi
   fi
 }
 # Función principal
 function nginx_config() {
-  echo "**********NEXTCLOUD CONFIG***********"
+  echo "**********NGINX CONFIG***********"
   mkdir
   echo "*************ALL DONE**************"
 }
+# Llamar a la funcion princial
+nginx_confi
