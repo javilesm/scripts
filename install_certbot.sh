9cb8f7fb4fe2af7a119c1b5f859ec53e62527eda
javilesm <68437746+javilesm@users.noreply.github.com>
2023-05-02 22:31:20 -0600

    Create install_certbot.sh

---
+++ install_certbot.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+# install_certbot.sh
+
+# Función para actualizar el sistema
+function actualizar_sistema() {
+  echo "Actualizando sistema..."
+  if sudo apt-get update -y; then
+    echo "Sistema actualizado."
+  else
+    echo "Error al actualizar el sistema."
+    exit 1
+  fi
+}
+
+# Función para instalar certbot
+function instalar_certbot() {
+  echo "Instalando Certbot..."
+  if sudo apt-get install certbot -y; then
+    echo "Certbot instalado correctamente."
+  else
+    echo "Error al instalar Certbot."
+    exit 1
+  fi
+}
+
+# Función principal
+function main() {
+  echo "**********INSTALACIÓN DE CERTBOT**********"
+  if actualizar_sistema && instalar_certbot; then
+    echo "**********INSTALACIÓN COMPLETADA**********"
+  else
+    echo "**********ERROR DURANTE LA INSTALACIÓN**********"
+    exit 1
+  fi
+}
+
+# Llamar a la función principal
+main
