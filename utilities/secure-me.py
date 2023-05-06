e303ef02ad355c20b5e2dacfa8ed0f3dabab2e72
javilesm <68437746+javilesm@users.noreply.github.com>
2023-05-05 19:44:29 -0600

    Update secure-me.py

--- utilities/secure-me.py
+++ utilities/secure-me.py
@@ -15,11 +15,18 @@ def generate_password(length):
 current_dir = os.path.dirname(os.path.abspath(__file__))
 users_file = os.path.join(current_dir, "users.csv")
 
-# Solicitar el nombre de usuario, la base de datos y el host deseado para cada usuario
+# Solicitar el nombre de usuario, el motor de base de datos, la base de datos y el host deseado para cada usuario
 username = input("Ingrese el nombre de usuario: ")
+db_engine = input("Ingrese el motor de base de datos deseado (1 para MySQL, cualquier otro valor para PostgreSQL): ")
 database = input("Ingrese el nombre de la base de datos: ")
 host = input("Ingrese el host deseado (presione Enter para usar 'localhost'): ") or 'localhost'
 privilege = input("Ingrese el privilegio deseado (presione Enter para usar 'ALL PRIVILEGES'): ") or 'ALL PRIVILEGES'
+
+# Establecer el motor de base de datos
+if db_engine == "1":
+    db_engine_str = "MySQL"
+else:
+    db_engine_str = "PostgreSQL"
 
 # Generar una contraseña aleatoria
 password_length = 64
@@ -37,9 +44,9 @@ sha256.update(password_bytes)
 # Obtener el valor hexadecimal del hash
 password_hash = sha256.hexdigest()
 
-# Guardar el nombre de usuario, la contraseña, la base de datos, el host y el privilegio en un archivo csv
+# Guardar el nombre de usuario, la contraseña, el motor de base de datos, la base de datos, el host y el privilegio en un archivo csv
 with open(users_file, "a") as f:
-    f.write("{},{},{},{},{}\n".format(username, password, host, database, privilege))
+    f.write("{},{},{},{},{},{}\n".format(username, password, db_engine_str, host, database, privilege))
 
 # Imprimir un mensaje de confirmación
 print("El nombre de usuario y la información de la base de datos se han almacenado en el archivo 'usuarios.csv'.")
@@ -49,6 +56,10 @@ user_file = os.path.join(current_dir, "{}.txt".format(username))
 with open(user_file, "w") as f:
     f.write("Nombre de usuario: {}\n".format(username))
     f.write("Contraseña: {}\n".format(password))
+    f.write("Motor de base de datos: {}\n".format(db_engine_str))
+    f.write("Base de datos: {}\n".format(database))
+    f.write("Host: {}\n".format(host))
+    f.write("Privilegio: {}\n".format(privilege))
 
 # Imprimir un mensaje de confirmación
 print("El archivo de texto para el usuario '{}' se ha creado en el directorio actual.".format(username))
