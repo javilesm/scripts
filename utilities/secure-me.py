0ed5bbf0e939864c95bc92c3a8367ded5d177bf3
javilesm <68437746+javilesm@users.noreply.github.com>
2023-05-05 20:55:30 -0600

    Update secure-me.py

--- utilities/secure-me.py
+++ utilities/secure-me.py
@@ -1,22 +1,14 @@
 import os
 import hashlib
+import random
 from Crypto.Random import get_random_bytes
 from random import choice
 import string
 
 def generate_password(length):
-    # Definir los caracteres especiales permitidos
-    special_chars = string.punctuation.replace(",", "")
-    
-    # Definir los demás caracteres permitidos
+    special_chars = "!\#$%&()*+<=>?@[\\]^_{|}~"
     other_chars = string.ascii_letters + string.digits
-    
-    # Generar una cantidad de bytes igual a la mitad de la longitud deseada
-    num_bytes = (length + 1) // 2
-    rand_bytes = get_random_bytes(num_bytes)
-
-    # Seleccionar caracteres aleatorios de ambas listas y combinarlos en una sola cadena
-    password = ''.join([choice(special_chars + other_chars) for i in range(length)])
+    password = ''.join([random.choice(special_chars + other_chars.replace(",", "")) for i in range(length)])
     return password
 
 # Obtener la ruta del archivo usuarios.csv
