8679de8e98d40e777a93f7908b92d72e2e9a187d
javilesm <68437746+javilesm@users.noreply.github.com>
2023-05-08 17:44:57 -0600

    Create postfix_config.sh

---
+++ Postfix/postfix_config.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+# postfix_config.sh
+# Variables 
+source get_ip.sh
+# Función principal
+function postfix_config() {
+   echo "La dirección IP de la máquina es: $ip_address"
+
+}
+# Llamar a la función principal
+postfix_config
