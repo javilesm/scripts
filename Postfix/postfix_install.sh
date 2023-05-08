91b78523b0e7b3c99e1b825b98fea0a81bdfb8bc
javilesm <68437746+javilesm@users.noreply.github.com>
2023-05-08 17:40:27 -0600

    Create postfix_install.sh

---
+++ Postfix/postfix_install.sh
@@ -0,0 +1,87 @@
+#!/bin/bash
+# dovecot_install.sh
+# Variables
+CURRENT_PATH="$( cd "$( dirname "${0}" )" && pwd )" # Obtener el directorio actual
+CONFIG_FILE="postfix_config.sh" # Script configurador
+CONFIG_PATH="$CURRENT_PATH/$CONFIG_FILE"
+# Function for installing the core components of Dovecot
+function install_packages() {
+  install_and_restart postfix
+  install_and_restart postfix-sqlite
+}
+# Función para instalar un paquete y reiniciar los servicios afectados
+function install_and_restart() {
+  local package="$1"
+  # Verificar si el paquete ya está instalado
+  echo "Verificando si el paquete '$package' ya está instalado..."
+  if dpkg -s "$package" >/dev/null 2>&1; then
+    echo "El paquete '$package' ya está instalado."
+    return 0
+  fi
+
+  # Instalar el paquete
+  echo "Instalando el paquete '$package'..."
+  if ! sudo apt-get install "$package" -y; then
+    echo "ERROR: no se pudo instalar el paquete '$package'."
+    return 1
+  fi
+  
+   # Verificar si el paquete se instaló correctamente
+   echo "Verificando si el paquete '$package' se instaló correctamente..."
+  if [ $? -eq 0 ]; then
+    echo "$package se ha instalado correctamente."
+  else
+    echo "ERROR: Error al instalar el paquete '$package'."
+    return 1
+  fi
+  
+  # Buscar los servicios que necesitan reiniciarse
+  echo "Buscando los servicios que necesitan reiniciarse..."
+  services=$(systemctl list-dependencies --reverse "$package" | grep -oP '^\w+(?=.service)')
+
+  # Reiniciar los servicios que dependen del paquete instalado
+  echo "Reiniciando los servicios que dependen del paquete instalado..."
+  if [[ -n $services ]]; then
+    echo "Reiniciando los siguientes servicios: $services"
+    if ! sudo systemctl restart $services; then
+      echo "Error: no se pudieron reiniciar los servicios después de instalar el paquete '$package'."
+      return 1
+    fi
+  else
+    echo "No se encontraron servicios que necesiten reiniciarse después de instalar el paquete '$package'."
+  fi
+
+  echo "El paquete '$package' se instaló correctamente."
+  return 0
+}
+# Función para verificar si el archivo de configuración existe
+function validate_config_file() {
+  echo "Verificando si el archivo de configuración existe..."
+  if [ ! -f "$CONFIG_PATH" ]; then
+    echo "ERROR: El archivo de configuración '$CONFIG_FILE' no se puede encontrar en la ruta '$CONFIG_PATH'."
+    exit 1
+  fi
+  echo "El archivo de configuración '$CONFIG_FILE' existe."
+}
+# Función para ejecutar el configurador de Postfix
+function run_config() {
+  echo "Ejecutar el configurador '$CONFIG_FILE'..."
+    # Intentar ejecutar el archivo de configuración de Postfix
+  if sudo bash "$CONFIG_PATH"; then
+    echo "El archivo de configuración '$CONFIG_FILE' se ha ejecutado correctamente."
+  else
+    echo "ERROR: No se pudo ejecutar el archivo de configuración '$CONFIG_FILE'."
+    exit 1
+  fi
+  echo "Configurador '$CONFIG_FILE' ejecutado."
+}
+# funcion principal
+function dovecot_install() {
+    echo "***************POSTFIX INSTALL***************"
+    install_packages
+    validate_config_file
+    run_config
+    echo "***************ALL DONE***************"
+}
+# Llamar a la funcion principal
+dovecot_install
