7657825ab974c1b6b1cf5da3568fbecef12093c7
javilesm <68437746+javilesm@users.noreply.github.com>
2023-05-11 22:02:19 -0600

    Update postfix_vmailbox.sh

--- Postfix/postfix_vmailbox.sh
+++ Postfix/postfix_vmailbox.sh
@@ -4,9 +4,12 @@
 CURRENT_DIR="$( cd "$( dirname "${0}" )" && pwd )" # Obtener el directorio actual
 ACCOUNTS_FILE="mail_users.csv"
 ACCOUNTS_PATH="$CURRENT_DIR/$ACCOUNTS_FILE"
+DOMAINS_FILE="domains.txt"
+DOMAINS_PATH="$CURRENT_DIR/$DOMAINS_FILE"
 POSTFIX_PATH="/etc/postfix"
 VMAILBOX_FILE="vmailbox" # archivo de buzones virtuales
 VMAILBOX_PATH="$POSTFIX_PATH/$VMAILBOX_FILE"
+VIRTUAL_ALIAS="$POSTFIX_PATH/virtual"
 # Función para verificar si el archivo de dominios existe
 function validate_accounts_file() {
     # verificar si el archivo de dominios existe
@@ -20,28 +23,43 @@ function validate_accounts_file() {
 
 # Función para leer la lista de direcciones de correo
 function read_accounts() {
-    # leer la lista de direcciones de correo
-    echo "Leyendo la lista de dominios '$ACCOUNTS_PATH'..."
-    while IFS="," read -r username nombre apellido email alias password; do
-        echo "Usario: $username"
-        echo "Dirección: $alias"
-        echo "Contraseña: ${password:0:3}*********"
-        # Obtener el dominio del correo electrónico (todo lo que está después del símbolo @)
-        domain="${alias#*@}"
-        # Escribir una entrada en el archivo de buzones virtuales para el usuario y el dominio
-        echo "\"$username@$domain\" \"$domain/$username/\""
-        echo "$username@$domain $domain/$username/" >> "$VMAILBOX_PATH"
-        echo "Los datos del usuario '$username' han sido registrados en '$VMAILBOX_PATH'"
-        echo "++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++"
-    done < <(grep -v '^$' "$ACCOUNTS_PATH")
-    echo "Todas las cuentas de correo han sido copiadas."
+  # leer la lista de direcciones de correo
+  echo "Leyendo la lista de dominios '$ACCOUNTS_PATH'..."
+  while IFS="," read -r username nombre apellido email alias password; do
+      echo "Usario: $username"
+      echo "Dirección: $alias"
+      echo "Contraseña: ${password:0:3}*********"
+      # Obtener el dominio del correo electrónico (todo lo que está después del símbolo @)
+      local domain="${alias#*@}"
+      # Escribir una entrada en el archivo de buzones virtuales para el usuario y el dominio
+      echo "\"$username@$domain\" \"$domain/$username/\""
+      echo "$username@$domain $domain/$username/" >> "$VMAILBOX_PATH"
+      echo "Los datos del usuario '$username' han sido registrados en '$VMAILBOX_PATH'"
+      echo "++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++"
+  done < <(grep -v '^$' "$ACCOUNTS_PATH")
+  echo "Todas las cuentas de correo han sido copiadas."
 }
-
+# Función para leer la lista de dominios y generar los archivos de índice para los archivos de alias virtual y buzones virtuales. 
+function create_index() {
+  # leer la lista de dominios
+  echo "Leyendo la lista de dominios..."
+  while read -r domain; do
+    # generar los archivos de índice para el archivo de alias virtual
+    echo "Generando los archivos de índice para el alias virtual: $domain"
+    echo "$VIRTUAL_ALIAS/$domain"
+    #postmap "$VIRTUAL_ALIAS/$domain"
+    echo "++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++"
+  done < <(sed -e '$a\' "$DOMAINS_PATH")
+  echo "Todos los archivos de índice han sido generados."
+  echo "$VMAILBOX_PATH"
+  #postmap "$VMAILBOX_PATH"
+}
 # Función principal
 function postfix_vmailbox() {
   echo "***************POSTFIX ACCOUNTS***************"
   validate_accounts_file
   read_accounts
+  create_index
   echo "***************ALL DONE***************"
 }
 # Llamar a la función principal
