50fd19589b8dffd7706039fca76500cd36706789
javilesm <68437746+javilesm@users.noreply.github.com>
2023-05-12 14:39:03 -0600

    Update mysql_install.sh

--- MySQL/mysql_install.sh
+++ MySQL/mysql_install.sh
@@ -5,33 +5,6 @@ CONFIG_FILE="mysql_config.sh"
 CURRENT_PATH="$( cd "$( dirname "${0}" )" && pwd )" # Obtener el directorio actual
 CONFIG_PATH="$CURRENT_PATH/$CONFIG_FILE"
 
-# Función para verificar si el archivo de configuración de MySQL existe
-function check_mysql_config_file() {
-echo "Verificando si el archivo de configuración de MySQL existe..."
-  if [[ ! -f /etc/mysql/mysql.conf.d/mysqld.cnf ]]; then
-    echo "El archivo de configuración de MySQL no existe."
-    exit 1
-  fi
-  echo "El archivo de configuración de MySQL existe."
-}
-# Función para realizar una copia de seguridad de mysql.conf
-function backup_mysql_config_file() {
-  echo "Realizando una copia de seguridad de mysqld.cnf..."
-  if ! sudo cp /etc/mysql/mysql.conf.d/mysqld.cnf /etc/mysql/mysql.conf.d/mysqld.cnf.bak; then
-    echo "Error al realizar una copia de seguridad de mysqld.cnf."
-    exit 1
-  fi
-  echo "Copia de seguridad de mysqld.cnf realizada."
-}
-# Función para modificar el archivo de configuración y permitir conexiones desde cualquier IP
-function modify_mysql_config_file() {
-  echo "Modificando el archivo de configuración y permitir conexiones desde cualquier IP..."
-  if ! sudo sed -i 's/bind-address.*/bind-address = 0.0.0.0/' /etc/mysql/mysql.conf.d/mysqld.cnf; then
-    echo "Error al modificar el archivo de configuración."
-    exit 1
-  fi
-  echo "El archivo de configuración fue modificado."
-}
 # Función para verificar si el archivo de configuración existe
 function validate_config_file() {
   echo "Verificando si el archivo de configuración existe..."
@@ -57,9 +30,6 @@ function mysql_config() {
 # Función principal
 function mysql_install() {
     echo "*******MYSQL INSTALL******"
-    check_mysql_config_file
-    backup_mysql_config_file
-    modify_mysql_config_file
     validate_config_file
     mysql_config
     echo "*********ALL DONE********"
