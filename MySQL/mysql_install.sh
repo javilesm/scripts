74d32987ebcd58c801966f6ddcf8c9636451598b
javilesm <68437746+javilesm@users.noreply.github.com>
2023-05-12 14:37:07 -0600

    Update mysql_install.sh

--- MySQL/mysql_install.sh
+++ MySQL/mysql_install.sh
@@ -5,56 +5,6 @@ CONFIG_FILE="mysql_config.sh"
 CURRENT_PATH="$( cd "$( dirname "${0}" )" && pwd )" # Obtener el directorio actual
 CONFIG_PATH="$CURRENT_PATH/$CONFIG_FILE"
 
-# Función para instalar MySQL
-function install_mysql () {
-  install_and_restart mysql-server
-}
-
-# Función para instalar un paquete y reiniciar los servicios afectados
-function install_and_restart() {
-  local package="$1"
-  # Verificar si el paquete ya está instalado
-  echo "Verificando si el paquete ya está instalado..."
-  if dpkg -s "$package" >/dev/null 2>&1; then
-    echo "El paquete '$package' ya está instalado."
-    return 0
-  fi
-
-  # Instalar el paquete
-  echo "Instalando $package..."
-  if ! sudo apt-get install "$package" -y; then
-    echo "Error: no se pudo instalar el paquete '$package'."
-    return 1
-  fi
-  
-   # Verificar si el paquete se instaló correctamente
-   echo "Verificando si el paquete se instaló correctamente..."
-  if [ $? -eq 0 ]; then
-    echo "$package se ha instalado correctamente."
-  else
-    echo "Error al instalar $package."
-    return 1
-  fi
-  
-  # Buscar los servicios que necesitan reiniciarse
-  echo "Buscando los servicios que necesitan reiniciarse..."
-  services=$(systemctl list-dependencies --reverse "$package" | grep -oP '^\w+(?=.service)')
-
-  # Reiniciar los servicios que dependen del paquete instalado
-  echo "Reiniciando los servicios que dependen del paquete instalado..."
-  if [[ -n $services ]]; then
-    echo "Reiniciando los siguientes servicios: $services"
-    if ! sudo systemctl restart $services; then
-      echo "Error: no se pudieron reiniciar los servicios después de instalar el paquete '$package'."
-      return 1
-    fi
-  else
-    echo "No se encontraron servicios que necesiten reiniciarse después de instalar el paquete '$package'."
-  fi
-
-  echo "El paquete '$package' se instaló correctamente."
-  return 0
-}
 # Función para verificar si el archivo de configuración de MySQL existe
 function check_mysql_config_file() {
 echo "Verificando si el archivo de configuración de MySQL existe..."
@@ -107,7 +57,6 @@ function mysql_config() {
 # Función principal
 function mysql_install() {
     echo "*******MYSQL INSTALL******"
-    install_mysql
     check_mysql_config_file
     backup_mysql_config_file
     modify_mysql_config_file
