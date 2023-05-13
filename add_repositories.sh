39edd4445ad9401556fb38b25cd8371b5a4824ee
javilesm <68437746+javilesm@users.noreply.github.com>
2023-05-12 19:08:25 -0600

    Update add_repositories.sh

--- add_repositories.sh
+++ add_repositories.sh
@@ -8,11 +8,13 @@ REPO_PATH="$CURRENT_PATH/$REPO_FILE"
 function add_repositories() {
     echo "Agregando repositorios..."
     while read -r repository; do
-        if ! yes '' | sudo add-apt-repository -y "$repository"; then
-            echo "No se pudo agregar el repositorio: $repository"
+        echo "Agregando el repositorio: '$repository'"
+        if ! yes '' | sudo add-apt-repository -y -r"$repository"; then
+            echo "ERROR: No se pudo agregar el repositorio '$repository'"
             exit 1
         fi
     done < "$REPO_PATH"
+    echo "Todos los repositorios fueron agregados."
 }
 # Lllamar a la función principal
 add_repositories
