102bb72ca83887ace4a2145f497cb0931b4e241e
javilesm <68437746+javilesm@users.noreply.github.com>
2023-05-15 11:23:03 -0600

    Update postfix_accounts.sh

--- Postfix/postfix_accounts.sh
+++ Postfix/postfix_accounts.sh
@@ -15,6 +15,15 @@ function validate_accounts_file() {
   fi
   echo "El archivo de dominios '$ACCOUNTS_FILE' existe."
 }
+# Función para verificar si el archivo virtual existe
+function validate_virtual_file() {
+  echo "Verificando si el archivo virtual existe..."
+  if [ ! -f "$POSTFIX_PATH/virtual" ]; then
+    echo "Creando archivo virtual en '$POSTFIX_PATH/virtual'..."
+    touch "$POSTFIX_PATH/virtual"
+  fi
+  echo "El archivo virtual existe."
+}
 # Función para leer la lista de direcciones de correo
 function read_accounts() {
     # leer la lista de direcciones de correo
@@ -31,11 +40,26 @@ function read_accounts() {
     done < <(grep -v '^$' "$ACCOUNTS_PATH")
     echo "Todas las cuentas de correo han sido copiadas."
 }
+# Función para reiniciar el servicio de Postfix y el servicio de Dovecot
+function restart_postfix() {
+    # reiniciar el servicio de Postfix
+    echo "Restarting Postfix service..."
+    sudo service postfix restart || { echo "Error: Failed to restart Postfix service."; return 1; }
+    echo "Postfix service restarted successfully."
+    sudo service postfix status || { echo "Error: Failed to check Postfix status."; return 1; }
+    # reiniciar el servicio de Dovecot
+    echo "Restarting Dovecot service..."
+    sudo service dovecot restart || { echo "Error: Failed to restart Dovecot service."; return 1; }
+    echo "Dovecot service restarted successfully."
+    sudo service dovecot status || { echo "Error: Failed to check Dovecot status."; return 1; }
+}
 # Función principal
 function postfix_accounts() {
   echo "***************POSTFIX ACCOUNTS***************"
   validate_accounts_file
+  validate_virtual_file
   read_accounts
+  restart_postfix
   echo "***************ALL DONE***************"
 }
 # Llamar a la función principal
