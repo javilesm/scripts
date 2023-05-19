8f3914b66f984df0b6c05faf0190fa1959def59d
javilesm <68437746+javilesm@users.noreply.github.com>
2023-05-19 11:50:32 -0600

    Update postfix_accounts.sh

--- Postfix/postfix_accounts.sh
+++ Postfix/postfix_accounts.sh
@@ -7,6 +7,7 @@ ACCOUNTS_PATH="$CURRENT_DIR/$ACCOUNTS_FILE"
 DOMAINS_FILE="domains.txt"
 DOMAINS_PATH="$CURRENT_DIR/$DOMAINS_FILE"
 POSTFIX_PATH="/etc/postfix"
+MAIL_PATH="/var/spool/mail"
 # Función para verificar si el archivo de dominios existe
 function validate_accounts_file() {
     # verificar si el archivo de dominios existe
@@ -19,15 +20,28 @@ function validate_accounts_file() {
 }
 # Función para verificar si la ruta virtual existe
 function validate_virtual_path() {
-  echo "Verificando si la ruta virtual existe..."
+  echo "Verificando si la ruta '$POSTFIX_PATH/virtual' existe..."
   if [ ! -d "$POSTFIX_PATH/virtual" ]; then
     echo "Creando la ruta: '$POSTFIX_PATH/virtual'..."
     sudo mkdir -p "$POSTFIX_PATH/virtual"
+    sudo chmod 777 "$POSTFIX_PATH/virtual"
   else
-    echo "La ruta virtual ya existe."
+    echo "La ruta '$POSTFIX_PATH/virtual' ya existe."
   fi
 }
-
+# Función para verificar si la ruta virtual existe
+function validate_mail_path() {
+  echo "Verificando si la ruta '$MAIL_PATH' existe..."
+  if [ ! -d "$MAIL_PATH" ]; then
+    # crear directorio
+    echo "Creando el directorio: '$MAIL_PATH'..."
+    sudo mkdir -p "$MAIL_PATH"
+    # cambiar permisos al directorio
+    sudo chmod 777 "$MAIL_PATH"
+  else
+    echo "La ruta '$MAIL_PATH' ya existe."
+  fi
+}
 # Función para verificar si el archivo /etc/dovecot/users existe
 function validate_users_file() {
   echo "Verificando si el archivo de usuarios existe..."
@@ -43,6 +57,10 @@ function create_files() {
     # leer la lista de dominios
     echo "Leyendo la lista de dominios: '$DOMAINS_PATH'..."
     while read -r host; do
+      # crear directorios
+      sudo mkdir -p "$MAIL_PATH/$host"
+      # cambiar permisos al directorio
+      sudo chmod 777 "$MAIL_PATH/$host"
       # Creando archivos para virtual_alias
       sudo touch "$POSTFIX_PATH/virtual/$host"
     done < <(grep -v '^$' "$DOMAINS_PATH")
@@ -64,10 +82,13 @@ function read_accounts() {
       # Escribiendo datos 
       echo "$username@$domain $domain/$username" | grep -v '^$' >> "$POSTFIX_PATH/virtual/$domain"
       echo "Los datos del usuario '$username' han sido registrados en: '$POSTFIX_PATH/virtual/$domain'"
-      # crear directorios para cada usuario dentro de /var/mail/vhosts/
+      # crear directorios para cada usuario dentro de /var/spool/mail/$domain
+      sudo mkdir -p "$MAIL_PATH/$domain/$username"
+      sudo chmod 777 "$MAIL_PATH/$domain/$username"
+      # crear directorios para cada usuario dentro de /var/mail/vhosts/$domain
       sudo mkdir "/var/mail/vhosts/$domain/$alias"
       sudo chown postfix:mail "/var/mail/vhosts/$domain/$alias"
-      sudo chmod 770 "/var/mail/vhosts/$domain/$alias"
+      sudo chmod 777 "/var/mail/vhosts/$domain/$alias"
       # agregar las cuentas de correo junto con sus contraseñas
       echo "Agregando las cuentas de correo junto con sus contraseñas..."
       echo "$alias:{PLAIN}$password" | grep -v '^$' >> "/etc/dovecot/users"
@@ -106,6 +127,7 @@ function postfix_accounts() {
   echo "***************POSTFIX ACCOUNTS***************"
   validate_accounts_file
   validate_virtual_path
+  validate_mail_path
   validate_users_file
   create_files
   read_accounts
