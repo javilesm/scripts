80ca1ad77cd0369e1b1ee3465b2e6193603f713c
javilesm <68437746+javilesm@users.noreply.github.com>
2023-05-23 20:18:34 -0600

    Update postfix_accounts.sh

--- Postfix/postfix_accounts.sh
+++ Postfix/postfix_accounts.sh
@@ -7,27 +7,17 @@ ACCOUNTS_PATH="$CURRENT_DIR/$ACCOUNTS_FILE"
 DOMAINS_FILE="domains.txt"
 DOMAINS_PATH="$CURRENT_DIR/$DOMAINS_FILE"
 POSTFIX_PATH="/etc/postfix"
+DOVECOT_PATH="/etc/dovecot"
 MAIL_PATH="/var/mail"
-# Función para verificar si el archivo de dominios existe
+# Función para verificar si el archivo de cuentas de usuario existe
 function validate_accounts_file() {
     # verificar si el archivo de dominios existe
-  echo "Verificando si el archivo de dominios existe..."
+  echo "Verificando si el archivo de cuentas de usuario existe..."
   if [ ! -f "$ACCOUNTS_PATH" ]; then
-    echo "ERROR: El archivo de dominios '$ACCOUNTS_FILE' no se puede encontrar en la ruta '$ACCOUNTS_PATH'."
+    echo "ERROR: El archivo de cuentas de usuario '$ACCOUNTS_FILE' no se puede encontrar en la ruta '$ACCOUNTS_PATH'."
     exit 1
   fi
-  echo "El archivo de dominios '$ACCOUNTS_FILE' existe."
-}
-# Función para verificar si la ruta virtual existe
-function validate_virtual_path() {
-  echo "Verificando si la ruta '$POSTFIX_PATH/virtual' existe..."
-  if [ ! -d "$POSTFIX_PATH/virtual" ]; then
-    echo "Creando la ruta: '$POSTFIX_PATH/virtual'..."
-    sudo mkdir -p "$POSTFIX_PATH/virtual"
-    sudo chmod 777 "$POSTFIX_PATH/virtual"
-  else
-    echo "La ruta '$POSTFIX_PATH/virtual' ya existe."
-  fi
+  echo "El archivo de cuentas de usuario '$ACCOUNTS_FILE' existe."
 }
 # Función para verificar si la ruta virtual existe
 function validate_mail_path() {
@@ -36,7 +26,8 @@ function validate_mail_path() {
     # crear directorio
     echo "Creando el directorio: '$MAIL_PATH'..."
     sudo mkdir -p "$MAIL_PATH"
-    # cambiar permisos al directorio
+    # cambiar permisos del directorio padre
+    echo "Cambiando los permisos del directorio padre '$MAIL_PATH'..."
     sudo chmod 777 "$MAIL_PATH"
   else
     echo "La ruta '$MAIL_PATH' ya existe."
@@ -45,12 +36,28 @@ function validate_mail_path() {
 # Función para verificar si el archivo /etc/dovecot/users existe
 function validate_users_file() {
   echo "Verificando si el archivo de usuarios existe..."
-  if [ ! -f "/etc/dovecot/users" ]; then
-    echo "Creando archivo '/etc/dovecot/users'..."
-    cd "/etc/dovecot"
-    sudo touch "users"
+  if [ ! -f "$DOVECOT_PATH/users" ]; then
+    echo "Creando archivo '$DOVECOT_PATH/users'..."
+    sudo touch "$DOVECOT_PATH/users"
   fi
-  echo "El archivo '/etc/dovecot/users' ha sido creado."
+  echo "El archivo '$DOVECOT_PATH/users' ha sido creado."
+}
+# Función para leer la lista de direcciones de dominios y mapear  las direcciones y destinos
+function read_domains() {
+    # leer la lista de dominios
+    echo "Leyendo la lista de dominios: '$DOMAINS_PATH'..."
+    while read -r host; do
+        # crear subdirectorios para cada dominio
+      echo "Cambiando los permisos del directorio padre '$MAIL_PATH'..."
+      sudo mkdir -p "$MAIL_PATH/$host"
+      # cambiar permisos del subdirectorio
+      echo "Cambiando los permisos del subdirectorio '$MAIL_PATH/$host'..."
+      sudo chmod 700 "$MAIL_PATH/$host"
+      # cambiar la propiedad del directorio
+      echo "Cambiando la propiedad del directorio '$MAIL_PATH/$host'..."
+      sudo chown 120:128 "$MAIL_PATH/$host"
+    done < <(grep -v '^$' "$DOMAINS_PATH")
+    echo "Todas los permisos y propiedades han sido actualizados."
 }
 # Función para leer la lista de direcciones de correo
 function read_accounts() {
@@ -64,15 +71,17 @@ function read_accounts() {
       # Obtener el dominio del correo electrónico (todo lo que está después del símbolo @)
       local domain="${alias#*@}"
       echo "Dominio: $domain"
+      # crear subdirectorio de la cuenta
+      sudo mkdir -p "$MAIL_PATH/$domain/$username"
       # Escribir una entrada en el archivo de buzones virtuales para el usuario y el dominio
-      echo "$username@$domain $domain/$username"
+      echo "$username@$domain $username.$domain"
       # Escribiendo datos 
       echo "$username@$domain $domain/$username" | grep -v '^$' >> "$POSTFIX_PATH/virtual_alias_maps"
       echo "Los datos del usuario '$username' han sido registrados en: '$POSTFIX_PATH/virtual_alias_maps'"
       # agregar las cuentas de correo junto con sus contraseñas
       echo "Agregando las cuentas de correo junto con sus contraseñas..."
-      echo "$alias:{PLAIN}$password" | grep -v '^$' >> "/etc/dovecot/users"
-      echo "Los datos del usuario '$username' han sido registrados en: '/etc/dovecot/users'"
+      echo "$alias:{PLAIN}$password" | grep -v '^$' >> "$DOVECOT_PATH/users"
+      echo "Los datos del usuario '$username' han sido registrados en: '$DOVECOT_PATH/users'"
       echo "++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++"
     done < <(grep -v '^$' "$ACCOUNTS_PATH")
     echo "Todas las cuentas de correo han sido copiadas."
@@ -80,23 +89,6 @@ function read_accounts() {
     echo "Mapeando las direcciones y usuarios..."
     sudo postmap "$POSTFIX_PATH/virtual_alias_maps" || { echo "Error: Failure while executing postmap on: '$POSTFIX_PATH/virtual_alias_maps'"; return 1; }
 
-}
-# Función para leer la lista de direcciones de dominios y mapear  las direcciones y destinos
-function read_domains() {
-    # leer la lista de dominios
-    echo "Leyendo la lista de dominios: '$DOMAINS_PATH'..."
-    while read -r host; do
-      # cambiar permisos del directorio padre
-      echo "Cambiando los permisos del directorio padre '/var/mail'..."
-      sudo chmod 755 "/var/mail"
-      # cambiar permisos del subdirectorio
-      echo "Cambiando los permisos del subdirectorio '/var/mail/$host'..."
-      sudo chmod 700 "/var/mail/$host"
-      # cambiar la propiedad del directorio
-      echo "Cambiando la propiedad del directorio '/var/mail/$host'..."
-      sudo chown 120:128 "/var/mail/$host"
-    done < <(grep -v '^$' "$DOMAINS_PATH")
-    echo "Todas los permisos y propiedades han sido actualizados."
 }
 # Función para reiniciar el servicio de Postfix y el servicio de Dovecot
 function restart_services() {
@@ -114,11 +106,10 @@ function restart_services() {
 function postfix_accounts() {
   echo "***************POSTFIX ACCOUNTS***************"
   validate_accounts_file
-  validate_virtual_path
   validate_mail_path
   validate_users_file
-  read_accounts
   read_domains
+  read_accounts
   restart_services
   echo "***************ALL DONE***************"
 }
