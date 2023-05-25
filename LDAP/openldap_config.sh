44664d0d8dc228cbc8e4f70492776e5aa0847670
javilesm <68437746+javilesm@users.noreply.github.com>
2023-05-25 10:25:43 -0600

    Update openldap_config.sh

--- LDAP/openldap_config.sh
+++ LDAP/openldap_config.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 # openldap_config.sh
 # Variables
+COMPANY="samava"
 DOMAIN="avilesworks.com"
-PASSWORD="Mexico2023-"
+ADMIN_PASSWORD="Mexico2023-"
 function configurar_openldap() {
   # Configuración inicial de OpenLDAP
   echo "Configuración inicial de OpenLDAP..."
@@ -10,15 +11,20 @@ function configurar_openldap() {
 
   # Respuestas a las preguntas del asistente de configuración
   sudo debconf-set-selections <<EOF
-slapd slapd/password1 password "$PASSWORD"
-slapd slapd/password2 password "$PASSWORD"
+slapd slapd/password1 password "$ADMIN_PASSWORD"
+slapd slapd/password2 password "$ADMIN_PASSWORD"
+slapd shared/organization string "$COMPANY"
 slapd slapd/domain string "$DOMAIN"
 EOF
 
   # Iniciar configuración inicial
   sudo dpkg-reconfigure -f noninteractive slapd
 }
-
+function restart_service() {
+  # Reiniciar el servicio slapd
+  echo "Reiniciando el servicio slapd..."
+  sudo systemctl restart slapd
+}
 function verificar_estado() {
   # Verificar estado del servicio
   echo "Verificando el estado del servicio..."
@@ -28,6 +34,7 @@ function verificar_estado() {
 # Funcion principal
 function openldap_config() {
   configurar_openldap
+  restart_service
   verificar_estado
 }
 # Llamar a la funcion principal
