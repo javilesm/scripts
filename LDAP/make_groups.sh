db385c0086a50d13462b1369adeb1f55826eb759
javilesm <68437746+javilesm@users.noreply.github.com>
2023-05-29 12:41:02 -0600

    Create make_groups.sh

---
+++ LDAP/make_groups.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+# make_groups.sh
+# Variables
+CURRENT_DIR="$( cd "$( dirname "${0}" )" && pwd )" # Obtener el directorio actual
+PARENT_DIR="$( dirname "$CURRENT_DIR" )" # Get the parent directory of the current directory
+DOMAINS_FILE="domains.txt"
+DOMAINS_PATH="$PARENT_DIR/Postfix/$DOMAINS_FILE"
+GROUPS_FILE="grupos.ldif"
+GROUPS_PATH="$CURRENT_DIR/$GROUPS_FILE"
+MAIL_DIR="/var/mail"
+
+# Función para crear el archivo base de usuarios, leer la lista de usuarios y escribir la informacion del usuario en el archivo base de usuarios
+function read_groups() {
+  echo "Creando el archivo base de usuarios '$GROUPS_PATH'..."
+  sudo touch "$GROUPS_PATH"
+  
+  local uidNumber=10001
+  local gidNumber=10001
+  
+  while IFS="," read -r hostname; do
+    echo "Hostname: $hostname"
+     
+    local domain="${hostname#*@}"
+    domain="${domain%%.*}"
+    echo "Dominio: $domain"
+  
+    local top_level="${hostname##*.}"
+    echo "Top level: $top_level"
+  
+    echo "Escribiendo la información de cada dominio en el archivo base de grupos..."
+    # Escribir la estructura para cada dominio en el archivo base de grupos
+    echo "dn: ou=People,dc=$domain,dc=$top_level" >> "$GROUPS_PATH"
+    echo "objectClass: organizationalUnit" >> "$GROUPS_PATH"
+    echo "ou: People" >> "$GROUPS_PATH"
+    echo >> "$GROUPS_PATH"
+
+  done < <(grep -v '^$' "$DOMAINS_PATH")
+  
+  echo "Todas las cuentas de correo han sido copiadas."
+}
+
+function make_groups() {
+    read_groups
+}
+# Llamar a la funcion principal
+make_groups
