65584e496c3249342ec07c7580d0e64f075a3980
javilesm <68437746+javilesm@users.noreply.github.com>
2023-05-30 10:39:20 -0600

    Update make_groups.sh

--- LDAP/make_groups.sh
+++ LDAP/make_groups.sh
@@ -10,12 +10,29 @@ GROUPS_PATH="$CURRENT_DIR/$GROUPS_FILE"
 LDAP_USERS_PATH="/etc/postfix/ldap-users.cf"
 LDAP_ALIASES_PATH="/etc/postfix/ldap-aliases.cf"
 MAIL_DIR="/var/mail"
+# Función para crear el archivo base de usuarios
+function create_groups_file() {
+  # Verificar si el archivo ya existe
+  if [ -e "$GROUPS_PATH" ]; then
+    echo "El archivo base de usuarios '$GROUPS_PATH' ya existe."
+    return 1
+  fi
 
-# Función para crear el archivo base de usuarios, leer la lista de usuarios y escribir la informacion del usuario en el archivo base de usuarios
-function make_grupos() {
+  # Crear el archivo base de usuarios
   echo "Creando el archivo base de usuarios '$GROUPS_PATH'..."
-  sudo touch "$GROUPS_PATH"
-  
+  if sudo touch "$GROUPS_PATH"; then
+    echo "Archivo base de usuarios creado correctamente."
+    return 0
+  else
+    echo "Error al crear el archivo base de usuarios."
+    return 1
+  fi
+}
+
+# Función para leer la lista de usuarios y escribir la estructura para cada dominio en el archivo base de grupos
+function make_grupos() {
+  # leer la lista de usuarios
+  echo "Leeyendo la lista de usuarios '$GROUPS_PATH'..."
   while IFS="," read -r hostname; do
     echo "Hostname: $hostname"
      
@@ -26,7 +43,7 @@ function make_grupos() {
     local top_level="${hostname##*.}"
     echo "Top level: $top_level"
   
-    echo "Escribiendo la información de cada dominio en el archivo base de grupos..."
+    echo "Escribiendo la estructura para cada dominio en el archivo base de grupos..."
     # Escribir la estructura para cada dominio en el archivo base de grupos
     echo "dn: ou=People,dc=$domain,dc=$top_level" >> "$GROUPS_PATH"
     echo "objectClass: organizationalUnit" >> "$GROUPS_PATH"
@@ -38,11 +55,29 @@ function make_grupos() {
   
   echo "Todas las cuentas de correo han sido copiadas."
 }
-# Función para crear el archivo base de usuarios, leer la lista de usuarios y escribir la informacion del usuario en el archivo base de usuarios
-function make_ldap_users() {
+# Función para crear el archivo base de usuarios
+function create_users_file() {
+  # Verificar si el archivo ya existe
+  if [ -e "$LDAP_USERS_PATH" ]; then
+    echo "El archivo base de usuarios '$LDAP_USERS_PATH' ya existe."
+    return 1
+  fi
+
+  # Crear el archivo base de usuarios
   echo "Creando el archivo base de usuarios '$LDAP_USERS_PATH'..."
-  sudo touch "$LDAP_USERS_PATH"
-  
+  if sudo touch "$LDAP_USERS_PATH"; then
+    echo "Archivo base de usuarios creado correctamente."
+    return 0
+  else
+    echo "Error al crear el archivo base de usuarios."
+    return 1
+  fi
+}
+
+# Función para leer la lista de usuarios y escribir la informacion del usuario en el archivo base de usuarios
+function make_ldap_users() {
+  # leer la lista de usuarios 
+  echo "Leyendo la lista de usuarios '$LDAP_USERS_PATH'..."
   while IFS="," read -r hostname; do
     echo "Hostname: $hostname"
      
@@ -68,14 +103,30 @@ function make_ldap_users() {
     echo "query_filter = (&(objectClass=inetOrgPerson)(mail=%s))" >> "$LDAP_USERS_PATH"
     echo "result_attribute = homeDirectory" >> "$LDAP_USERS_PATH"
     echo "result_filter = %s/Maildir/" >> "$LDAP_USERS_PATH"
-  done < <(grep -v '^$' "$LDAP_USERS_PATH")
+  done < <(grep -v '^$' "$DOMAINS_PATH")
   echo "Todas las cuentas de correo han sido copiadas."
 }
+# Función para crear el archivo base de usuarios
+function create_aliases_file() {
+  # Verificar si el archivo ya existe
+  if [ -e "$LDAP_ALIASES_PATH" ]; then
+    echo "El archivo base de usuarios '$LDAP_ALIASES_PATH' ya existe."
+    return 1
+  fi
+
+  # Crear el archivo base de usuarios
+  echo "Creando el archivo base de usuarios '$LDAP_ALIASES_PATH'..."
+  if sudo touch "$LDAP_ALIASES_PATH"; then
+    echo "Archivo base de usuarios creado correctamente."
+    return 0
+  else
+    echo "Error al crear el archivo base de usuarios."
+    return 1
+  fi
+}
+
 # Función para crear el archivo base de usuarios, leer la lista de usuarios y escribir la informacion del usuario en el archivo base de usuarios
 function make_ldap_aliases() {
-  echo "Creando el archivo base de usuarios '$LDAP_ALIASES_PATH'..."
-  sudo touch "$LDAP_ALIASES_PATH"
-  
   while IFS="," read -r hostname; do
     echo "Hostname: $hostname"
      
@@ -88,26 +139,31 @@ function make_ldap_aliases() {
   
     echo "Escribiendo la información de cada dominio en el archivo base de grupos..."
     # Escribir la estructura para cada dominio en el archivo base de grupos
-    echo "server_host = ldap://ldap.$hostname" >> "$LDAP_USERS_PATH"
-    echo "start_tls = yes" >> "$LDAP_USERS_PATH"
-    echo "tls_ca_cert_file = /etc/ldap/tls/CA.pem" >> "$LDAP_USERS_PATH"
-    echo "tls_require_cert = yes" >> "$LDAP_USERS_PATH"
-    echo "search_base = dc=$domain,dc=$top_level" >> "$LDAP_USERS_PATH"
-    echo "scope = sub" >> "$LDAP_USERS_PATH"
-    echo "version = 3" >> "$LDAP_USERS_PATH"
-    echo "bind = yes" >> "$LDAP_USERS_PATH"
-    echo "bind_dn = cn=admin,dc=$domain,dc=$top_level" >> "$LDAP_USERS_PATH"
-    echo "bind_pw = admin_password" >> "$LDAP_USERS_PATH"
-    echo "query_filter = (&(objectClass=inetOrgPerson)(mail=%s))" >> "$LDAP_USERS_PATH"
+    echo "server_host = ldap://ldap.$hostname" >> "$LDAP_ALIASES_PATH"
+    echo "start_tls = yes" >> "$LDAP_ALIASES_PATH"
+    echo "tls_ca_cert_file = /etc/ldap/tls/CA.pem" >> "$LDAP_ALIASES_PATH"
+    echo "tls_require_cert = yes" >> "$LDAP_ALIASES_PATH"
+    echo "search_base = dc=$domain,dc=$top_level" >> "$LDAP_ALIASES_PATH"
+    echo "scope = sub" >> "$LDAP_ALIASES_PATH"
+    echo "version = 3" >> "$LDAP_ALIASES_PATH"
+    echo "bind = yes" >> "$LDAP_ALIASES_PATH"
+    echo "bind_dn = cn=admin,dc=$domain,dc=$top_level" >> "$LDAP_ALIASES_PATH"
+    echo "bind_pw = admin_password" >> "$LDAP_ALIASES_PATH"
+    echo "query_filter = (&(objectClass=inetOrgPerson)(mail=%s))" >> "$LDAP_ALIASES_PATH"
     echo "result_attribute = mail" >> "$LDAP_ALIASES_PATH"
     echo "result_filter = %s" >> "$LDAP_ALIASES_PATH"
-  done < <(grep -v '^$' "$LDAP_ALIASES_PATH")
+  done < <(grep -v '^$' "$DOMAINS_PATH")
   echo "Todas las cuentas de correo han sido copiadas."
 }
 function make_groups() {
+  echo "***************MAKE GROUPS***************"
+  create_groups_file
   make_grupos
+  create_users_file
   make_ldap_users
+  create_aliases_file
   make_ldap_aliases
+  echo "***************ALL DONE***************"
 }
 # Llamar a la funcion principal
 make_groups
