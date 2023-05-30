22f67e5e8c06c7d648f8b574b72fda28a35d4c4e
javilesm <68437746+javilesm@users.noreply.github.com>
2023-05-29 20:14:12 -0600

    Update make_groups.sh

--- LDAP/make_groups.sh
+++ LDAP/make_groups.sh
@@ -7,10 +7,12 @@ DOMAINS_FILE="domains.txt"
 DOMAINS_PATH="$PARENT_DIR/Postfix/$DOMAINS_FILE"
 GROUPS_FILE="grupos.ldif"
 GROUPS_PATH="$CURRENT_DIR/$GROUPS_FILE"
+LDAP_USERS_PATH="/etc/postfix/ldap-users.cf"
+LDAP_ALIASES_PATH="/etc/postfix/ldap-aliases.cf"
 MAIL_DIR="/var/mail"
 
 # Función para crear el archivo base de usuarios, leer la lista de usuarios y escribir la informacion del usuario en el archivo base de usuarios
-function read_groups() {
+function make_grupos() {
   echo "Creando el archivo base de usuarios '$GROUPS_PATH'..."
   sudo touch "$GROUPS_PATH"
   
@@ -36,9 +38,68 @@ function read_groups() {
   
   echo "Todas las cuentas de correo han sido copiadas."
 }
-
+# Función para crear el archivo base de usuarios, leer la lista de usuarios y escribir la informacion del usuario en el archivo base de usuarios
+function make_ldap_users() {
+  echo "Creando el archivo base de usuarios '$LDAP_USERS_PATH'..."
+  sudo touch "$LDAP_USERS_PATH"
+  
+  while IFS="," read -r hostname; do
+    echo "Hostname: $hostname"
+     
+    local domain="${hostname#*@}"
+    domain="${domain%%.*}"
+    echo "Dominio: $domain"
+  
+    local top_level="${hostname##*.}"
+    echo "Top level: $top_level"
+  
+    echo "Escribiendo la información de cada dominio en el archivo base de grupos..."
+    # Escribir la estructura para cada dominio en el archivo base de grupos
+    echo "server_host = localhost" >> "$LDAP_USERS_PATH"
+    echo "search_base = dc=$domain,dc=$top_level" >> "$LDAP_USERS_PATH"
+    echo "version = 3" >> "$LDAP_USERS_PATH"
+    echo "bind = no" >> "$LDAP_USERS_PATH"
+    echo "bind_dn = cn=admin,dc=$domain,dc=$top_level" >> "$LDAP_USERS_PATH"
+    echo "bind_pw = admin_password" >> "$LDAP_USERS_PATH"
+    echo "query_filter = (&(objectClass=inetOrgPerson)(mail=%s))" >> "$LDAP_USERS_PATH"
+    echo "result_attribute = homeDirectory" >> "$LDAP_USERS_PATH"
+    echo "result_filter = %s/Maildir/" >> "$LDAP_USERS_PATH"
+  done < <(grep -v '^$' "$LDAP_USERS_PATH")
+  echo "Todas las cuentas de correo han sido copiadas."
+}
+# Función para crear el archivo base de usuarios, leer la lista de usuarios y escribir la informacion del usuario en el archivo base de usuarios
+function make_ldap_aliases() {
+  echo "Creando el archivo base de usuarios '$LDAP_ALIASES_PATH'..."
+  sudo touch "$LDAP_ALIASES_PATH"
+  
+  while IFS="," read -r hostname; do
+    echo "Hostname: $hostname"
+     
+    local domain="${hostname#*@}"
+    domain="${domain%%.*}"
+    echo "Dominio: $domain"
+  
+    local top_level="${hostname##*.}"
+    echo "Top level: $top_level"
+  
+    echo "Escribiendo la información de cada dominio en el archivo base de grupos..."
+    # Escribir la estructura para cada dominio en el archivo base de grupos
+    echo "server_host = localhost" >> "$LDAP_ALIASES_PATH"
+    echo "search_base = dc=$domain,dc=$top_level" >> "$LDAP_ALIASES_PATH"
+    echo "version = 3" >> "$LDAP_ALIASES_PATH"
+    echo "bind = no" >> "$LDAP_ALIASES_PATH"
+    echo "bind_dn = cn=admin,dc=$domain,dc=$top_level" >> "$LDAP_ALIASES_PATH"
+    echo "bind_pw = admin_password" >> "$LDAP_ALIASES_PATH"
+    echo "query_filter = (&(objectClass=inetOrgPerson)(mail=%s))" >> "$LDAP_ALIASES_PATH"
+    echo "result_attribute = mail" >> "$LDAP_ALIASES_PATH"
+    echo "result_filter = %s" >> "$LDAP_ALIASES_PATH"
+  done < <(grep -v '^$' "$LDAP_ALIASES_PATH")
+  echo "Todas las cuentas de correo han sido copiadas."
+}
 function make_groups() {
-    read_groups
+  make_grupos
+  make_ldap_users
+  make_ldap_aliases
 }
 # Llamar a la funcion principal
 make_groups
