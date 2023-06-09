2cc2bfc9e806bfe68ef24eb71ac846f52eb8222e
javilesm <68437746+javilesm@users.noreply.github.com>
2023-06-09 08:16:03 -0600

    Update nextcloud_config.sh

--- NEXTCLOUD/nextcloud_config.sh
+++ NEXTCLOUD/nextcloud_config.sh
@@ -6,6 +6,7 @@ NGINX_SITES_ENABLED="/etc/nginx/sites-enabled/"
 HTML_PATH="/var/www"
 PARENT_DIR="$( dirname "$CURRENT_PATH" )" # Get the parent directory of the current directory
 host="nextcloud"
+$server_ip="3.220.58.75"
 NGINX_NEXTCLOUD_CONFIG="/etc/nginx/sites-available/$host"
 site_root="$HTML_PATH/$host"
 GID="10000"
@@ -44,11 +45,26 @@ function restart_nginx() {
   echo "Reiniciando servicio Nginx..."
   sudo service nginx restart
 }
-# Funcion para remover el archivo de configuracion
-function remove_default_config() {
-  # remover el archivo de configuracion
-  echo "Removiendo archivo de configuracion..."
-  sudo rm "$NGINX_SITES_ENABLED/default"
+
+function get_php_fpm_version() {
+    # Buscar la ubicación del binario de php-fpm
+    php_fpm_path=$(whereis -b php-fpm | awk '{print $2}')
+
+    if [ -z "$php_fpm_path" ]; then
+        echo "PHP-FPM no encontrado en el sistema."
+        return
+    fi
+
+    # Obtener la versión de PHP-FPM
+    version_output=$("$php_fpm_path" -v 2>&1)
+    regex="PHP ([0-9]+\.[0-9]+)"
+
+    if [[ $version_output =~ $regex ]]; then
+        version_number="${BASH_REMATCH[1]}"
+        echo "Versión de PHP-FPM: $version_number"
+    else
+        echo "No se pudo obtener la versión de PHP-FPM."
+    fi
 }
 # Funcion para configurar Nginx
 function configure_nginx() {
@@ -117,10 +133,119 @@ function create_nginx_configs() {
   # Editar el archivo de configuración
   echo "Editando el archivo de configuración..."
   echo "server {
-  listen 80;
-  server_name 3.220.58.75;
-  root $site_root;
-  index index.html index.php;
+    listen 80;
+    listen [::]:80;
+    server_name $server_ip;
+
+    # Add headers to serve security related headers
+    add_header X-Content-Type-Options nosniff;
+    add_header X-XSS-Protection "1; mode=block";
+    add_header X-Robots-Tag none;
+    add_header X-Download-Options noopen;
+    add_header X-Permitted-Cross-Domain-Policies none;
+    add_header Referrer-Policy no-referrer;
+
+    #I found this header is needed on Ubuntu, but not on Arch Linux. 
+    add_header X-Frame-Options "SAMEORIGIN";
+
+    # Path to the root of your installation
+    root $site_root;
+
+    access_log /var/log/nginx/nextcloud.access;
+    error_log /var/log/nginx/nextcloud.error;
+
+location = /robots.txt {
+        allow all;
+        log_not_found off;
+        access_log off;
+}
+
+    # The following 2 rules are only needed for the user_webfinger app.
+    # Uncomment it if you're planning to use this app.
+    #rewrite ^/.well-known/host-meta /public.php?service=host-meta last;
+    #rewrite ^/.well-known/host-meta.json /public.php?service=host-meta-json
+    # last;
+
+location = /.well-known/carddav {
+        return 301 $scheme://$host/remote.php/dav;
+}
+    
+location = /.well-known/caldav {
+       return 301 $scheme://$host/remote.php/dav;
+}
+
+location ~ /.well-known/acme-challenge {
+      allow all;
+}
+
+    # set max upload size
+    client_max_body_size 512M;
+    fastcgi_buffers 64 4K;
+
+    # Disable gzip to avoid the removal of the ETag header
+    gzip off;
+
+    # Uncomment if your server is build with the ngx_pagespeed module
+    # This module is currently not supported.
+    #pagespeed off;
+
+    error_page 403 /core/templates/403.php;
+    error_page 404 /core/templates/404.php;
+
+location / {
+       rewrite ^ /index.php;
+}
+
+location ~ ^/(?:build|tests|config|lib|3rdparty|templates|data)/ {
+       deny all;
+}
+
+location ~ ^/(?:\.|autotest|occ|issue|indie|db_|console) {
+       deny all;
+ }
+
+location ~ ^/(?:index|remote|public|cron|core/ajax/update|status|ocs/v[12]|updater/.+|ocs-provider/.+|core/templates/40[34])\.php(?:$|/) {
+       include fastcgi_params;
+       fastcgi_split_path_info ^(.+\.php)(/.*)$;
+       try_files $fastcgi_script_name =404;
+       fastcgi_param SCRIPT_FILENAME $document_root$fastcgi_script_name;
+       fastcgi_param PATH_INFO $fastcgi_path_info;
+       #Avoid sending the security headers twice
+       fastcgi_param modHeadersAvailable true;
+       fastcgi_param front_controller_active true;
+       fastcgi_pass unix:/run/php/php$version_number-fpm.sock;
+       fastcgi_intercept_errors on;
+       fastcgi_request_buffering off;
+}
+
+location ~ ^/(?:updater|ocs-provider)(?:$|/) {
+       try_files $uri/ =404;
+       index index.php;
+}
+
+    # Adding the cache control header for js and css files
+    # Make sure it is BELOW the PHP block
+location ~* \.(?:css|js)$ {
+        try_files $uri /index.php$uri$is_args$args;
+        add_header Cache-Control "public, max-age=7200";
+        # Add headers to serve security related headers (It is intended to
+        # have those duplicated to the ones above)
+        add_header X-Content-Type-Options nosniff;
+        add_header X-XSS-Protection "1; mode=block";
+        add_header X-Robots-Tag none;
+        add_header X-Download-Options noopen;
+        add_header X-Permitted-Cross-Domain-Policies none;
+        add_header Referrer-Policy no-referrer;
+        # Optional: Don't log access to assets
+        access_log off;
+ }
+
+location ~* \.(?:svg|gif|png|html|ttf|woff|ico|jpg|jpeg)$ {
+        try_files $uri /index.php$uri$is_args$args;
+        # Optional: Don't log access to other assets
+        access_log off;
+}
+   
 }" | sudo tee "$NGINX_NEXTCLOUD_CONFIG" > /dev/null
 
 }
@@ -130,6 +255,8 @@ function test_config() {
   if sudo nginx -t; then
     echo "Nginx se ha configurado correctamente."
     sudo service nginx reload
+    sudo service php"$version_number"-fpm restart
+    sudo service php"$version_number"-fpm status
   else
     echo "ERROR: Hubo un problema con la configuración de Nginx."
     exit 1
@@ -179,6 +306,7 @@ function nextcloud_config() {
   read_KEY_PATH
   uninstall_apache2
   restart_nginx
+  get_php_fpm_version
   #configure_nginx
   create_nginx_configs
   test_config
