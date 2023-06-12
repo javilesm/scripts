c3f9d9562cf8b32b23aec22f145280f5305a5fb6
javilesm <68437746+javilesm@users.noreply.github.com>
2023-06-11 19:41:55 -0600

    Update nginx_config.sh

--- NGINX/nginx_config.sh
+++ NGINX/nginx_config.sh
@@ -129,7 +129,8 @@ function create_nginx_configs() {
     index index.php;
 
     location / {
-        try_files \$uri \$uri/ /index.php?\$args;
+        #try_files \$uri \$uri/ =404;
+        try_files \$uri \$uri/ /index.php\$is_args\$args;
     }
 
     location ~ \.php$ {
