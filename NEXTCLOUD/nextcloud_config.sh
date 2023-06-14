05adcb41e33f6acaf812e0737da8b7b7de0dc6f0
javilesm <68437746+javilesm@users.noreply.github.com>
2023-06-13 22:10:21 -0600

    Update nextcloud_config.sh

--- NEXTCLOUD/nextcloud_config.sh
+++ NEXTCLOUD/nextcloud_config.sh
@@ -3,13 +3,17 @@
 # Variables
 CURRENT_PATH="$( cd "$( dirname "${0}" )" && pwd )" # Obtener el directorio actual
 NGINX_SITES_ENABLED="/etc/nginx/sites-enabled/"
-HTML_PATH="/var/www"
 PARENT_DIR="$( dirname "$CURRENT_PATH" )" # Get the parent directory of the current directory
-host="nextcloud"
+host="samava-cloud"
+nextcloud_dir="nextcloud"
+react_dir="react-app"
+django_dir="django"
 server_ip="3.220.58.75"
+HTML_PATH="/var/www/$host"
 config_path="/etc/nginx/sites-available/$host"
-site_root="$HTML_PATH/$host"
-GID="10000"
+nextcloud_root="$HTML_PATH/$nextcloud_dir"
+react_root="$HTML_PATH/$react_dir"
+django_root="$HTML_PATH/$django_dir"
 GID_NAME="www-data"
 UID_NAME="www-data"
 CERTS_FILE="generate_certs.sh"
@@ -59,62 +63,6 @@ function get_php_fpm_version() {
     fi
 }
 
-# Funcion para configurar Nginx
-function configure_nginx() {
-  echo "Configurando NGINX..."
-  
-  if ! sudo tee "$config_path" >/dev/null <<EOF
-  server {
-    listen 80;
-    server_name '$DOMAIN';
-    return 301 https://\$host\$request_uri;
-  }
-
-  server {
-    listen 443 ssl http2;
-    server_name '$DOMAIN';
-
-    ssl_certificate /etc/letsencrypt/live/'$DOMAIN'/fullchain.pem;
-    ssl_certificate_key /etc/letsencrypt/live/'$DOMAIN'/privkey.pem;
-
-    add_header X-Content-Type-Options nosniff;
-    add_header X-Frame-Options "SAMEORIGIN";
-    add_header X-XSS-Protection "1; mode=block";
-
-    location / {
-      proxy_pass http://127.0.0.1:80;
-      proxy_set_header Host \$host;
-      proxy_set_header X-Real-IP \$remote_addr;
-      proxy_set_header X-Forwarded-Proto https;
-      proxy_set_header X-Forwarded-For \$proxy_add_x_forwarded_for;
-      proxy_set_header X-Forwarded-Host \$server_name;
-      proxy_set_header X-Forwarded-Port \$server_port;
-      proxy_set_header X-Forwarded-Ssl on;
-      proxy_set_header Upgrade \$http_upgrade;
-      proxy_set_header Connection "Upgrade";
-    }
-
-    location = /robots.txt {
-      allow all;
-      log_not_found off;
-      access_log off;
-    }
-
-    location ^~ /.well-known {
-      allow all;
-      log_not_found off;
-      access_log off;
-    }
-
-    client_max_body_size 0;
-  }
-EOF
-  then
-    echo "Error: No se pudo escribir la configuración de NGINX en el archivo."
-    return 1
-  fi
-  echo "Configuración de NGINX exitosa."
-}
 function create_nginx_configs() {
   # Crear el archivo de configuración
   echo "Creando archivos de configuración para el dominio: $host..."
@@ -127,84 +75,93 @@ function create_nginx_configs() {
   # Editar el archivo de configuración
   
   echo "server {
-  listen 80;
-  server_name $server_ip;
-  root $site_root;
-  index index.php;
-  return 301 https://\$server_name\$request_uri;
+    listen 80;
+    server_name $server_ip;
+    return 301 https://\$host\$request_uri;       
 }
 
 server {
     listen 443 ssl;
     server_name $server_ip;
-
+    
     # Configura los certificados SSL
-    ssl_certificate $PEM_PATH;
-    ssl_certificate_key $KEY_PATH;
+    ssl_certificate  $PEM_PATH;
+    ssl_certificate_key  $KEY_PATH;
 
-    # Configura la ubicación de los archivos de Nextcloud
-    root $site_root;
-    index index.php;
+    root $HTML_PATH/html;
+    index index.html;
 
-    # Configura las reglas de reescritura de URL
-    rewrite ^/.well-known/carddav /remote.php/dav/ permanent;
-    rewrite ^/.well-known/caldav /remote.php/dav/ permanent;
-    rewrite ^(/core/doc/[^\/]+/)$ \$1/index.html;
-
-    # Configura la ubicación de los archivos de registro
-    access_log /var/log/nginx/nextcloud.access.log;
-    error_log /var/log/nginx/nextcloud.error.log;  
-
-    # Configura la ubicación de los archivos de caché
-location = /robots.txt {
-  allow all;
-  log_not_found off;
-  access_log off;
-}
-
-    # Configura la ubicación de los archivos de caché
-location ~ ^/(?:\.htaccess|data|config|db_structure\.xml|README) {
-  deny all;
-}
-
-    # Configura las reglas de reescritura de URL para PHP
-location ~ ^\/(?:index|remote|public|cron|core\/ajax\/update|status|ocs\/v[12]|updater\/.+|oc[ms]-provider\/.+)\.php(?:$|\/) {
-  fastcgi_split_path_info ^(.+\.php)(\/.+)$;
-  fastcgi_pass unix:/var/run/php/php$version_number-fpm.sock;
-  fastcgi_index index.php;
-  include fastcgi_params;
-  fastcgi_param SCRIPT_FILENAME \$document_root\$fastcgi_script_name;
-  fastcgi_param PATH_INFO \$fastcgi_path_info;
-  fastcgi_param HTTPS on;
-}
-
- # Configura las reglas de reescritura de URL para otros archivos PHP
-location ~ ^\/(?:updater|oc[ms]-provider)(?:$|\/) {
-  try_files \$uri/ =404;
-  index index.php;
-}
-
-  # Configura las reglas de reescritura de URL para otros archivos
-location ~ \.(?:css|js|svg|gif|png|html|ttf|woff|ico|jpg|jpeg)$ {
-  try_files \$uri /index.php\$request_uri;
-  expires 30d;
-  access_log off;
-}
-
-# Configuración para servir el admin panel de Django
-location /admin {
-        proxy_pass http://127.0.0.1:8000;
-        proxy_set_header Host $host;
-        proxy_set_header X-Real-IP $remote_addr;
-        proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
-        proxy_set_header X-Forwarded-Proto $scheme;
+    location / {
+        try_files \$uri \$uri/ =404;
     }
- 
-# Configuración para servir archivos estáticos de Django
-location /static/ {
-        alias $HTML_PATH/django;
+
+    location /nextcloud {
+        root $nextcloud_root;
+        index index.php;
+    
+
+        location ~ \.php\$ {
+            include fastcgi_params;
+            fastcgi_param SCRIPT_FILENAME \$request_filename;
+            fastcgi_pass unix:/var/run/php/php$version_number-fpm.sock;
+        }
+        
     }
     
+    location /app {
+        root $react_root;
+        index index.js;
+    }
+    
+    # Configura la ubicación de los archivos de caché
+    location ~ ^/(?:\.htaccess|data|config|db_structure\.xml|README) {
+        deny all;
+    }
+
+    # Configura las reglas de reescritura de URL
+    location ~ ^/.well-known/carddav {
+        rewrite ^(.*) /remote.php/dav/ redirect;
+    }
+
+    location ~ ^/.well-known/caldav {
+        rewrite ^(.*) /remote.php/dav/ redirect;
+    }
+
+    location ~ ^(/core/doc/[^\/]+/)$ {
+        rewrite ^(.*) \$1/index.html;
+    }
+
+    # Configura las reglas de reescritura de URL para otros archivos
+    location ~* \.(?:css|js|svg|gif|png|html|ttf|woff|ico|jpg|jpeg)\$ {
+        try_files \$uri \$uri/ =404;
+        expires 30d;
+        access_log off;
+    }
+
+    # Configura las reglas de reescritura de URL para PHP
+    location ~ ^\/(?:index|remote|public|cron|core\/ajax\/update|status|ocs\/v[12]|updater\/.+|oc[ms]-provider\/.+)\.php(?:\$|\/) {
+        fastcgi_split_path_info ^(.+\.php)(\/.+)\$;
+        fastcgi_param PATH_INFO \$fastcgi_path_info;
+        fastcgi_pass unix:/var/run/php/php$version_number-fpm.sock;
+        include fastcgi_params;
+        fastcgi_param SCRIPT_FILENAME \$document_root\$fastcgi_script_name;
+        fastcgi_param HTTPS on;
+    }
+
+    # Configura las reglas de reescritura de URL para otros archivos PHP
+    location ~ ^\/(?:updater|oc[ms]-provider)(?:\$|\/) {
+        try_files \$uri/ =404;
+        index index.php;
+    }
+
+    # Configura la ubicación de los archivos de caché
+    location = /robots.txt {
+        allow all;
+        log_not_found off;
+        access_log off;
+    }
+
+    # Configuración adicional según tus necesidades...
 }" | sudo tee "$config_path" > /dev/null
 
   echo "Archivo de configuración creado: $config_path"
@@ -226,11 +183,11 @@ function test_config() {
 }
 function webset() {
   # cambiar permisos del subdirectorio
-  echo "Cambiando los permisos del subdirectorio '$site_root'..."
-  sudo chmod -R 755 "$site_root"
+  echo "Cambiando los permisos del subdirectorio '$HTML_PATH'..."
+  sudo chmod -R 755 "$HTML_PATH"
   # cambiar la propiedad del directorio
-  echo "Cambiando la propiedad del directorio '$site_root'..."
-  sudo chown -R ${UID_NAME//\"/}:${GID_NAME//\"/} "$site_root"
+  echo "Cambiando la propiedad del directorio '$HTML_PATH'..."
+  sudo chown -R ${UID_NAME//\"/}:${GID_NAME//\"/} "$HTML_PATH"
   # create a symbolic link of the site configuration file in the sites-enabled directory.
   echo "Creando un vínculo simbólico del archivo '$config_path' y el archivo '$NGINX_SITES_ENABLED'..."
   if ! sudo ln -s "$config_path" "$NGINX_SITES_ENABLED"; then
@@ -256,7 +213,6 @@ function nextcloud_config() {
   uninstall_apache2
   restart_nginx
   get_php_fpm_version
-  #configure_nginx
   create_nginx_configs
   test_config
   webset
