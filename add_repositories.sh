63654a3a50dfe653f8185141fab5774ae545cae9
javilesm <68437746+javilesm@users.noreply.github.com>
2023-06-18 18:07:28 -0600

    Update add_repositories.sh

--- add_repositories.sh
+++ add_repositories.sh
@@ -6,18 +6,46 @@ CURRENT_PATH="$( cd "$( dirname "${0}" )" && pwd )" # Obtener el directorio actu
 REPO_PATH="$CURRENT_PATH/$REPO_FILE"
 # Función principal
 function add_repositories() {
+    if [[ ! -f "$REPO_PATH" ]]; then
+        echo "ERROR: El archivo '$REPO_FILE' no existe."
+        exit 1
+    fi
+    
     echo "Agregando repositorios..."
+    local count=0
+    local errors=0
+
     while read -r repository; do
+        if [[ -z "$repository" ]]; then
+            echo "ADVERTENCIA: Repositorio vacío. Saltando..."
+            continue
+        fi
+
+        ((count++))
         echo "Agregando el repositorio: '$repository'"
+        
         if ! yes '' | sudo add-apt-repository -y "$repository"; then
             echo "ERROR: No se pudo agregar el repositorio '$repository'"
+            ((errors++))
             if ! yes '' | sudo add-apt-repository -r "$repository"; then
-                echo "ERROR: No se pudo agregar el repositorio '$repository'"
+                echo "ERROR: No se pudo eliminar el repositorio '$repository'"
                 exit 1
             fi
         fi
     done < "$REPO_PATH"
-    echo "Todos los repositorios fueron agregados."
+    
+    echo "------------------------"
+    echo "Resumen de operación:"
+    echo "------------------------"
+    echo "Repositorios procesados: $count"
+    echo "Repositorios con errores: $errors"
+    
+    if [[ $errors -eq 0 ]]; then
+        echo "Todos los repositorios fueron agregados correctamente."
+    else
+        echo "Se produjeron errores durante la operación."
+    fi
 }
-# Lllamar a la función principal
+
+# Llamar a la función principal
 add_repositories
