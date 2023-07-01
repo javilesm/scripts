2cc69a90b4df7dc57e40aa7339d7240a614079c9
javilesm <68437746+javilesm@users.noreply.github.com>
2023-06-30 22:29:12 -0600

    Update phpmyadmin_install.sh

--- MySQL/phpmyadmin_install.sh
+++ MySQL/phpmyadmin_install.sh
@@ -19,15 +19,7 @@ function get_php_fpm_version() {
         echo "No se pudo obtener la versión de PHP-FPM."
     fi
 }
-# Leer los datos de configuración
-function read_config() {
-  if [[ ! -f "$CONFIG_FILE" ]]; then
-    echo "El archivo de configuración no existe: $CONFIG_FILE"
-    exit 1
-  fi
-  source "$CONFIG_FILE"
 
-}
 # Instalar phpMyAdmin
 function install_phpmyadmin() {
   sudo apt update
@@ -40,13 +32,6 @@ function config_phpmyadmin() {
   sudo phpenmod mbstring
 }
 
-# Actualizar la configuración de Nginx con los datos del archivo de configuración
-function update_config() {
-  sudo sed -i "s/your_db_host/$DB_HOST/g" "$NGINX_CONFIG"
-  sudo sed -i "s/your_db_user/$DB_USER/g" "$NGINX_CONFIG"
-  sudo sed -i "s/your_db_password/$DB_PASSWORD/g" "$NGINX_CONFIG"
-}
-
 # Reiniciar servicios
 function restart_services() {
   sudo systemctl restart nginx
@@ -54,10 +39,8 @@ function restart_services() {
 }
 function phpmyadmin_install() {
   get_php_fpm_version
-  read_config
   install_phpmyadmin
   config_phpmyadmin
-  update_config
   restart_services
 }
 phpmyadmin_install
