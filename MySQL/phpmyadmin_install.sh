81610020943122ccaea45da9a9f5daa199d5bff8
javilesm <68437746+javilesm@users.noreply.github.com>
2023-06-30 21:54:21 -0600

    Update phpmyadmin_install.sh

--- MySQL/phpmyadmin_install.sh
+++ MySQL/phpmyadmin_install.sh
@@ -6,8 +6,18 @@ CONFIG_FILE="$CURRENT_DIR/config.txt"
 PHPMYADMIN_PATH="/usr/share/phpmyadmin"
 WEB_DIR="/var/www/samava-cloud/phpmyadmin"
 
-function get_php_version() {
+function get_php_fpm_version() {
+    # Obtener la versión de PHP-FPM
+    version_output=$(php -v 2>&1)
+    regex="PHP ([0-9]+\.[0-9]+)"
 
+    if [[ $version_output =~ $regex ]]; then
+        version_number="${BASH_REMATCH[1]}"
+        PHP_VERSION=$version_number  # Asignar la versión a la variable PHP_VERSION
+        echo "Versión de PHP-FPM instalada: $PHP_VERSION"
+    else
+        echo "No se pudo obtener la versión de PHP-FPM."
+    fi
 }
 # Leer los datos de configuración
 function read_config() {
@@ -28,7 +38,6 @@ function install_phpmyadmin() {
 function config_phpmyadmin() {
   sudo ln -s "$PHPMYADMIN_PATH" "$WEB_DIR"
   sudo phpenmod mbstring
-  sudo systemctl restart php7.4-fpm
 }
 
 # Actualizar la configuración de Nginx con los datos del archivo de configuración
@@ -41,9 +50,10 @@ function update_config() {
 # Reiniciar servicios
 function restart_services() {
   sudo systemctl restart nginx
+  sudo systemctl restart php$PHP_VERSION-fpm
 }
 function phpmyadmin_install() {
-  get_php_version
+  get_php_fpm_version
   read_config
   install_phpmyadmin
   config_phpmyadmin
