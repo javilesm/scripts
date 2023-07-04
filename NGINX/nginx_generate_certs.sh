9698e7b99965f97c0c26f3ff5f035db59074f6a6
javilesm <68437746+javilesm@users.noreply.github.com>
2023-07-03 21:59:35 -0600

    Update nginx_generate_certs.sh

--- NGINX/nginx_generate_certs.sh
+++ NGINX/nginx_generate_certs.sh
@@ -4,6 +4,10 @@
 DOMAIN="avilesworks.com"
 NAME="server-cert"
 CERTS_PATH="/etc/nginx/ssl"
+CA_KEY="ca-key.pem"
+CA_KEY_PATH="$CERTS_PATH/$CA_KEY"
+CA_CERT="ca.pem"
+CA_CERT_PATH="$CERTS_PATH/$CA_CERT"
 KEY_FILE="$NAME.key"
 CSR_FILE="$NAME.csr"
 CRT_FILE="$NAME.crt"
@@ -27,6 +31,74 @@ function create_cert_directory() {
         echo "ERROR: Error al crear el directorio '$CERTS_PATH'. Verifica los permisos de superusuario."
         return 1
     fi
+    cd "$CERTS_PATH"
+}
+# Generate RSA
+function generate_ca_rsa() {
+    if [ -e "$CA_KEY_PATH" ]; then
+        echo "La llave '$CA_KEY_PATH' ya existe."
+        return 0
+    fi
+
+    # Generar la llave privada y certificado autofirmado
+    echo "Generando la llave privada..."
+    if sudo openssl genrsa -aes256 -out "$CA_KEY_PATH" 4096; then
+        echo "Se ha creado la llave: $CA_KEY_PATH."
+    else
+        echo "ERROR: Error al generar la llave '$CA_KEY_PATH'."
+        return 1
+    fi
+}
+function generate_ca_pem() {
+    if [ -e "$CA_CERT_PATH" ]; then
+        echo "La llave '$CA_CERT_PATH' ya existe."
+        return 0
+    fi
+
+    # Generar la llave privada y certificado autofirmado
+    echo "Generando la llave privada..."
+    if sudo openssl req -new -x509 -sha256 -days 3650 -key "$CA_KEY_PATH" -out "$CA_CERT_PATH" \
+        -subj "/C=US/ST=State/L=City/O=Organization/CN=$DOMAIN"; then
+        echo "Se ha creado la llave: $CA_CERT_PATH."
+    else
+        echo "ERROR: Error al generar la llave '$CA_CERT_PATH'."
+        return 1
+    fi
+}
+# Generate RSA key
+function generate_pem_file() {
+    if [ -e "$PEM_PATH" ]; then
+        echo "La llave '$PEM_PATH' ya existe."
+        return 0
+    fi
+
+    # Generar la llave privada y certificado autofirmado
+    echo "Generando la llave privada..."
+    if sudo openssl genrsa -out "$PEM_PATH" 4096; then
+        echo "Se ha creado la llave: $PEM_PATH."
+    else
+        echo "ERROR: Error al generar la llave '$PEM_PATH'."
+        return 1
+    fi
+}
+# Función para generar el requerimiento 
+function generate_csr_file() {
+    if [ -e "$CSR_PATH" ]; then
+        echo "El requerimiento '$CSR_PATH' ya existe."
+        return 0
+    fi
+    # Generar el requerimiento  
+    echo "Generando el requerimiento  ..."
+    if sudo openssl req -new -sha256 -key "$PEM_PATH" -out "$CSR_PATH" \
+        -subj "/C=US/ST=State/L=City/O=Organization/CN=$DOMAIN"; then
+        echo "Se ha creado el requerimiento: $CSR_PATH."
+    else
+        echo "ERROR: Error al generar el requerimiento '$CSR_PATH'."
+        return 1
+    fi
+}
+function create_extfile() {
+    echo "subjectAltName=DNS:*.$DOMAIN,IP:3.220.58.75" >> extfile.cnf
 }
 # Función para generar la llave privada 
 function generate_key() {
@@ -37,72 +109,28 @@ function generate_key() {
 
     # Generar la llave privada y certificado autofirmado
     echo "Generando la llave privada..."
-    if sudo openssl req -x509 -nodes -days 365 -newkey rsa:2048 \
-        -keyout "$KEY_PATH" -out "$CRT_PATH" \
-        -subj "/C=US/ST=State/L=City/O=Organization/CN=$DOMAIN"; then
+    if sudo openssl x509 -req -sha256 -days 365 -in "$CSR_PATH" -CA "$CA_CERT_PATH" -CAkey "$CA_KEY_PATH" -out cert.pem -extfile extfile.cnf -CAcreateserial; then
         echo "Se ha creado la llave: $KEY_PATH."
     else
         echo "ERROR: Error al generar la llave '$KEY_PATH'."
         return 1
     fi
 }
-# Función para generar el requerimiento 
-function generate_csr() {
-    if [ -e "$CSR_PATH" ]; then
-        echo "El requerimiento '$CSR_PATH' ya existe."
-        return 0
-    fi
-    # Generar el requerimiento  
-    echo "Generando el requerimiento  ..."
-    if sudo openssl req -new -key "$KEY_PATH" -out "$CSR_PATH" \
-        -subj "/C=US/ST=State/L=City/O=Organization/CN=$DOMAIN"; then
-        echo "Se ha creado el requerimiento: $CSR_PATH."
-    else
-        echo "ERROR: Error al generar el requerimiento '$CSR_PATH'."
-        return 1
-    fi
-}
-# Función para remover el passphrase de la llave 
-function remove_passphrase() {
-    echo "Removing passphrase from key (for nginx)..."
-    sudo cp "$KEY_PATH" "$KEY_PATH.org"
-    sudo openssl rsa -in "$KEY_PATH.org" -out "$KEY_PATH"
-    sudo rm "$KEY_PATH.org"
-}
-# Función para generar el certificado
-function generate_crt() {
-    if [ -e "$CRT_PATH" ]; then
-        echo "El certificado '$CRT_PATH' ya existe."
-        return 0
-    fi
-    # generar el certificado
-    echo "Generando el certificado..."
-    if openssl x509 -req -days 365 -in "$CSR_PATH" -signkey "$KEY_PATH" -out "$CRT_PATH"; then
-        echo "Se ha creado el certificado: '$CRT_PATH'."
-    else
-        echo "ERROR:Error al generar el certificado '$CRT_PATH'."
-        return 1
-    fi
-}
-
-# Función para cambiar permisos
-function change_mod() {
-    # cambiar permisos
-    echo "Cambiando permisos del archivo '$KEY_PATH'..."
-    sudo chmod 400 "$KEY_PATH"
-    echo "Cambiando permisos del archivo '$CRT_PATH'..."
-    sudo chmod 444 "$CRT_PATH"
-    echo "Los permisos fueron cambiados."
+function merge_certs() {
+    sudo cat cert.pem > fullchain.pem
+    sudo cat "$CA_CERT_PATH" >> .\fullchain.pem
 }
 # Función principal
 function nginx_generate_certs() {
     echo "******************NGINX GENERATE CERTS******************"
     create_cert_directory
+    generate_ca_rsa
+    generate_ca_pem
+    generate_pem_file
+    generate_csr_file
+    create_extfile
     generate_key
-    generate_csr
-    remove_passphrase
-    generate_crt
-    change_mod
+    merge_certs
     echo "******************ALL DONE******************"
 }
 # Llama a la función princial
