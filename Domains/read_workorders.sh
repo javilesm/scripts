9b79594320eecd4681dd9c412fbea372721c32e7
JORGE AVILES <68437746+javilesm@users.noreply.github.com>
2023-09-21 07:36:35 -0600

    Update read_workorders.sh

--- Domains/read_workorders.sh
+++ Domains/read_workorders.sh
@@ -6,13 +6,51 @@ PARENT_DIR="$(dirname "$CURRENT_DIR")"
 WORKORDERS_FILE="t_workorder.csv"
 WORKORDERSFILE_PATH="$CURRENT_DIR/$WORKORDERS_FILE"
 API_USER="6516516516" # Nuevo valor para el campo #12
+LOG_DATE=$(date +"%Y%m%d%H%M%S") # Fecha y hora actuales en formato AñoMesDíaHoraMinutoSegundo
+LOG_FILE="$CURRENT_DIR/read_workorders_$LOG_DATE.txt" # Nombre del archivo de registro con fecha y hora
+MYSQL_USER="$API_USER"
+MYSQL_PASSWORD="tu_contraseña"
+MYSQL_HOST="127.0.0.1"
+MYSQL_DATABASE="antares"
+MYSQL_TABLE="t_workorder"
 
-# Función para imprimir una línea de progreso
-print_progress() {
+# Función para conectarse a MySQL y verificar si la base de datos y la tabla existen
+check_mysql_table_existence() {
+  local db_user="$1"
+  local db_password="$2"
+  local db_host="$3"
+  local db_name="$4"
+  local table_name="$5"
+
+  # Comando para verificar si la base de datos existe
+  local check_db_query="SELECT SCHEMA_NAME FROM INFORMATION_SCHEMA.SCHEMATA WHERE SCHEMA_NAME='$db_name';"
+
+  # Ejecutar la consulta para verificar la existencia de la base de datos
+  if mysql -u "$db_user" -p"$db_password" -h "$db_host" -e "$check_db_query" 2>/dev/null | grep -q "$db_name"; then
+    echo "La base de datos $db_name existe."
+    
+    # Comando para verificar si la tabla existe
+    local check_table_query="SELECT 1 FROM information_schema.tables WHERE table_name='$table_name' AND table_schema='$db_name' LIMIT 1;"
+
+    # Intentar conectarse a MySQL y ejecutar la consulta para verificar la tabla
+    if mysql -u "$db_user" -p"$db_password" -h "$db_host" -e "$check_table_query" 2>/dev/null | grep -q 1; then
+      echo "La tabla $table_name existe en la base de datos $db_name."
+    else
+      echo "La tabla $table_name no existe en la base de datos $db_name."
+    fi
+  else
+    echo "La base de datos $db_name no existe."
+  fi
+}
+
+# Función para imprimir una línea de progreso y registrar en el archivo de log
+log_progress() {
   local completed="$1"
   local total="$2"
   local percentage=$((completed * 100 / total))
-  echo -ne "Progreso: $completed/$total ($percentage%)\r"
+  local log_message="Progreso: $completed/$total ($percentage%)"
+  echo -ne "$log_message\r"
+  echo "$(date +"%Y-%m-%d %H:%M:%S") - $log_message" >> "$LOG_FILE"
 }
 
 # Función para cambiar el registro si el valor #7 es igual a 1
@@ -34,7 +72,7 @@ change_record() {
     IFS=',' read -ra values <<< "$line"
     if [[ ${values[6]} -eq 1 ]]; then
       values[10]=$(date +"%d-%m-%Y %H:%M:%S")
-      values[6]=0
+      values[6]=3
       values[11]="$API_USER" # Modificar el valor #12 con el valor de API_USER
       ((modified_count++))
       modified_lines+=("${values[0]}") # Agregar el primer valor de la fila modificada
@@ -44,10 +82,12 @@ change_record() {
 
   echo "Filas modificadas: $modified_count"
   echo "Filas sin cambios: $unchanged_count"
-  echo "Filas modificadas (por referencia al primer valor):"
-  for line in "${modified_lines[@]}"; do
-    echo "$line"
-  done
+  echo "Filas modificadas (por referencia al primer valor):${modified_lines[*]}"
+
+  # Registrar eventos en el archivo de log
+  echo "$(date +"%Y-%m-%d %H:%M:%S") - Filas modificadas: $modified_count" >> "$LOG_FILE"
+  echo "$(date +"%Y-%m-%d %H:%M:%S") - Filas sin cambios: $unchanged_count" >> "$LOG_FILE"
+  echo "$(date +"%Y-%m-%d %H:%M:%S") - Filas modificadas (por referencia al primer valor):${modified_lines[*]}" >> "$LOG_FILE"
 }
 
 # Función principal para procesar el archivo CSV
@@ -77,5 +117,17 @@ process_csv() {
   mv "$temp_output_file" "$file"
 }
 
-# Llamar a la función principal para procesar el archivo CSV
-process_csv "$CURRENT_DIR" "$WORKORDERS_FILE"
+# Función principal (main)
+main() {
+  # Verificar si la tabla MySQL existe
+  check_mysql_table_existence "$MYSQL_USER" "$MYSQL_PASSWORD" "$MYSQL_HOST" "$MYSQL_DATABASE" "$MYSQL_TABLE"
+
+  # Crear o limpiar el archivo de registro de eventos
+  > "$LOG_FILE"
+
+  # Llamar a la función principal para procesar el archivo CSV
+  process_csv "$CURRENT_DIR" "$WORKORDERS_FILE"
+}
+
+# Llamar a la función principal
+main
