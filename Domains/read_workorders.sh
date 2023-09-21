455d065874832ac61f85f726951acb1ab6d8e7fd
JORGE AVILES <68437746+javilesm@users.noreply.github.com>
2023-09-20 21:42:35 -0600

    Update read_workorders.sh

--- Domains/read_workorders.sh
+++ Domains/read_workorders.sh
@@ -5,6 +5,7 @@ CURRENT_DIR="$(cd "$(dirname "${0}")" && pwd)"
 PARENT_DIR="$(dirname "$CURRENT_DIR")"
 WORKORDERS_FILE="t_workorder.csv"
 WORKORDERSFILE_PATH="$CURRENT_DIR/$WORKORDERS_FILE"
+API_USER="6516516516" # Nuevo valor para el campo #12
 
 # Función para imprimir una línea de progreso
 print_progress() {
@@ -14,6 +15,36 @@ print_progress() {
   echo -ne "Progreso: $completed/$total ($percentage%)\r"
 }
 
+# Función para cambiar el registro si el valor #7 es igual a 1
+change_record() {
+  local input_file="$1"
+  local output_file="$2"
+  local modified_count=0
+  local unchanged_count=0
+  local modified_lines=()
+
+  while IFS=',' read -r line; do
+    IFS=',' read -ra values <<< "$line"
+    if [[ ${values[6]} -eq 1 ]]; then
+      values[10]=$(date +"%d-%m-%Y %H:%M:%S")
+      values[6]=0
+      values[11]="$API_USER" # Modificar el valor #12 con el valor de API_USER
+      ((modified_count++))
+      modified_lines+=("${values[0]}") # Agregar el primer valor de la fila modificada
+    else
+      ((unchanged_count++))
+    fi
+    echo "${values[*]}" >> "$output_file"
+  done < "$input_file"
+
+  echo "Filas modificadas: $modified_count"
+  echo "Filas sin cambios: $unchanged_count"
+  echo "Filas modificadas (por referencia al primer valor):"
+  for line in "${modified_lines[@]}"; do
+    echo "$line"
+  done
+}
+
 # Función principal para procesar el archivo CSV
 process_csv() {
   local input_dir="$1"
@@ -21,27 +52,27 @@ process_csv() {
   local file="$input_dir/$filename"
   local total_lines=$(wc -l < "$file")
   local current_line=0
-  local skip_first_line=true
 
   echo "Leyendo el archivo CSV: $file"
-  echo "Total de filas: $((total_lines - 1))" # Restar 1 para omitir la primera línea
+  echo "Total de filas: $total_lines"
+  
+  # Mostrar la primera fila
+  head -n 1 "$file"
 
-  # Leer el archivo CSV línea por línea
-  while IFS= read -r line; do
-    ((current_line++))
-    if $skip_first_line; then
-      skip_first_line=false
-      continue  # Omitir la primera línea
-    fi
-    print_progress "$current_line" "$((total_lines - 1))" # Restar 1 para omitir la primera línea
-    echo "REGISTRO $current_line:"
-    IFS=',' read -ra values <<< "$line"
-    for ((i = 0; i < ${#values[@]}; i++)); do
-      echo "-- VALOR $((i + 1)): ${values[i]}"
-    done
-  done < "$file"
+  echo -e "\nDatos actuales en formato tabulado (incluyendo la primera fila):"
+  cat "$file"
 
-  echo -e "\nProceso completado."
+  local temp_output_file="/tmp/workorders_temp.csv"
+  > "$temp_output_file"
+
+  # Cambiar registros si el valor #7 es igual a 1
+  change_record "$file" "$temp_output_file"
+
+  echo -e "\nProceso de cambio de registros completado. Datos actualizados en formato tabulado:"
+  cat "$temp_output_file"
+
+  # Mover el archivo temporal al archivo de trabajo original
+  mv "$temp_output_file" "$file"
 }
 
 # Llamar a la función principal para procesar el archivo CSV
