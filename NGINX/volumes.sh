6b103f20a7e2cf552f6c5bd78406b7b95f09fef0
JORGE AVILES <68437746+javilesm@users.noreply.github.com>
2023-09-29 21:39:34 -0600

    Update volumes.sh

--- NGINX/volumes.sh
+++ NGINX/volumes.sh
@@ -5,6 +5,7 @@
 CURRENT_DIR="$(cd "$(dirname "${0}")" && pwd)"
 PARENT_DIR="$( dirname "$CURRENT_DIR" )" # Get the parent directory of the current directory
 CSV_TABLES_DIR="tablas"
+CSV_TABLES_PATH="$PARENT_DIR/Antares_project/$CSV_TABLES_DIR"
 CSV_PARTITIONS_FILE="t_partitions.csv"
 CSV_PARTITIONS_PATH="$CSV_TABLES_PATH/$CSV_PARTITIONS_FILE"
 
@@ -14,7 +15,64 @@ MYSQL_PASSWORD="antares1"
 MYSQL_HOST="localhost"  # Cambia a la dirección de tu servidor MySQL si es necesario
 MYSQL_DATABASE="antares"
 MYSQL_STORAGE_TABLE="t_storage"
-MYSQL_PARTITIONS_TABLE="t_partitions"
+MYSQL_PARTITIONS_TABLE="t_partition"
+
+# Función para obtener de manera dinamica los encabezados de la tabla t_storage en MYSQL
+function get_storage_headers() {
+    # obtener de manera dinamica los encabezados de la tabla t_storage en MYSQL
+    echo "Obteniendo de manera dinamica los encabezados de la tabla '$MYSQL_STORAGE_TABLE' en MYSQL..."
+    local mysql_query="SHOW COLUMNS FROM $MYSQL_STORAGE_TABLE"
+    local headers=($(mysql -u "$MYSQL_USER" -p"$MYSQL_PASSWORD" -h "$MYSQL_HOST" -D "$MYSQL_DATABASE" -s -N -e "$mysql_query" | cut -f1))
+    
+    if [ $? -eq 0 ]; then
+        echo "Encabezados de la tabla '$MYSQL_STORAGE_TABLE' obtenidos con éxito:"
+        echo "${headers[@]}"
+        return 0
+    else
+        echo "Error al obtener los encabezados de la tabla $MYSQL_STORAGE_TABLE."
+        return 1
+    fi
+}
+
+# Función para obtener de manera dinamica los encabezados de la tabla t_storage en MYSQL
+function get_partition_headers() {
+    # obtener de manera dinamica los encabezados de la tabla t_storage en MYSQL
+    echo "Obteniendo de manera dinamica los encabezados de la tabla '$MYSQL_PARTITIONS_TABLE' en MYSQL..."
+    local mysql_query="SHOW COLUMNS FROM $MYSQL_PARTITIONS_TABLE"
+    local headers=($(mysql -u "$MYSQL_USER" -p"$MYSQL_PASSWORD" -h "$MYSQL_HOST" -D "$MYSQL_DATABASE" -s -N -e "$mysql_query" | cut -f1))
+    
+    if [ $? -eq 0 ]; then
+        echo "Encabezados de la tabla '$MYSQL_PARTITIONS_TABLE' obtenidos con éxito:"
+        echo "${headers[@]}"
+        return 0
+    else
+        echo "Error al obtener los encabezados de la tabla $MYSQL_PARTITIONS_TABLE."
+        return 1
+    fi
+}
+
+# Función principal que consulta MySQL
+function read_storage_table() {
+    # Ejecutar la consulta SQL
+    SQL_QUERY="SELECT * FROM $MYSQL_STORAGE_TABLE"
+    MYSQL_RESULT=$(mysql -u "$MYSQL_USER" -p"$MYSQL_PASSWORD" -h "$MYSQL_HOST" -D "$MYSQL_DATABASE" -s -N -e "$SQL_QUERY")
+
+    if [ $? -eq 0 ]; then
+        # Procesar los resultados
+        echo "Lista de registros y su segunda columna:"
+        echo "------------------------------------------------------------------------------------------------"
+        while read -r record; do
+            third_value=$(echo "$record" | cut -f3)
+            echo "$record"
+            echo "******************************************"
+            get_disk_info $third_value
+            echo "******************************************"
+            echo "------------------------------------------------------------------------------------------------"
+        done <<< "$MYSQL_RESULT"
+    else
+        echo "Error al ejecutar la consulta SQL en MySQL."
+    fi
+}
 
 # Función para obtener información detallada sobre una unidad de disco
 function get_disk_info() {
@@ -37,32 +95,6 @@ function get_disk_info() {
             local partitioned_space=0
             local available_space=0
 
-            # Crear un archivo CSV para almacenar la información
-
-            # Comprobar si el archivo CSV ya existe
-            if [ ! -e "$CSV_PARTITIONS_PATH" ]; then
-                echo "Partición,Disco,Tamaño (bytes),Punto de montaje" > "$CSV_PARTITIONS_PATH"
-            fi
-
-            # Iterar a través de las particiones y dispositivos
-            for entry in $(echo "$lsblk_info" | jq -c '.blockdevices[0].children[]?'); do
-                local name=$(echo "$entry" | jq -r '.name')
-                local size=$(echo "$entry" | jq -r '.size')
-                local mountpoint=$(echo "$entry" | jq -r '.mountpoint')
-
-                # Comprobar si es una partición
-                if [ "$mountpoint" != "null" ]; then
-                    echo "Partición: $name"
-                    echo "Tamaño: $size bytes"
-                    echo "Punto de montaje: $mountpoint"
-                    ((partition_count++))
-                    ((partitioned_space += size))
-
-                    # Guardar información de la partición en el archivo CSV
-                    echo "$name,$device_name,$size,$mountpoint" >> "$CSV_PARTITIONS_PATH"
-                fi
-            done
-
             # Calcular espacio no particionado
             if [ $size -ge $partitioned_space ]; then
                 available_space=$((size - partitioned_space))
@@ -81,27 +113,12 @@ function get_disk_info() {
     fi
 }
 
-# Función principal que consulta MySQL
 function volumes() {
-    # Ejecutar la consulta SQL
-    SQL_QUERY="SELECT * FROM $MYSQL_STORAGE_TABLE"
-    MYSQL_RESULT=$(mysql -u "$MYSQL_USER" -p"$MYSQL_PASSWORD" -h "$MYSQL_HOST" -D "$MYSQL_DATABASE" -s -N -e "$SQL_QUERY")
-
-    if [ $? -eq 0 ]; then
-        # Procesar los resultados
-        echo "Lista de registros y su segunda columna:"
-        echo "------------------------------------------------------------------------------------------------"
-        while read -r record; do
-            third_value=$(echo "$record" | cut -f3)
-            echo "$record, segunda columna: $third_value"
-            echo "******************************************"
-            get_disk_info $third_value
-            echo "******************************************"
-            echo "------------------------------------------------------------------------------------------------"
-        done <<< "$MYSQL_RESULT"
-    else
-        echo "Error al ejecutar la consulta SQL en MySQL."
-    fi
+    get_storage_headers
+    echo "------------------------------------------------------------------------------------------------"
+    get_partition_headers
+    echo "------------------------------------------------------------------------------------------------"
+    read_storage_table
 }
 
 # Llamar a la función principal
