4ca4d1a3b54ef76fd66f6f9a10ab403e1eb6700b
JORGE AVILES <68437746+javilesm@users.noreply.github.com>
2023-10-16 20:14:55 -0600

    Update python_packages.sh

--- Python/python_packages.sh
+++ Python/python_packages.sh
@@ -9,6 +9,7 @@ ENVIRONMENTS_FILE="environments.txt"
 ENVIRONMENTS_PATH="$CURRENT_DIR/$ENVIRONMENTS_FILE"
 REPORT_FILE="report.txt"
 REPORT_PATH="$CURRENT_DIR/$REPORT_FILE"
+
 # Función para comprobar la existencia del archivo de paquetes pip
 function verify_list1() {
   if [ ! -f "$PACKAGES_PATH" ]; then
@@ -16,6 +17,7 @@ function verify_list1() {
     exit 1
   fi
 }
+
 # Función para comprobar la existencia del archivo de entornos
 function verify_list2() {
   if [ ! -f "$ENVIRONMENTS_PATH" ]; then
@@ -32,85 +34,39 @@ function check_report_file() {
     echo "Entorno    //    PIP" >> "$REPORT_PATH"
   fi
 }
-############################################################################################
-declare -A PACKAGES_PATH
-declare -A ENVIRONMENTS_PATH
 
-function read_lists() {
-  # Leer la lista de colores
+# Leer la lista de paquetes
+function read_packages() {
   if [[ -f "$PACKAGES_PATH" ]]; then
-    readarray -t paquetes < "$PACKAGES_PATH"
-    for i in "${!paquetes[@]}"; do
-      PACKAGES_PATH[$i]=${paquetes[$i]}
-    done
+    readarray -t packages < "$PACKAGES_PATH"
   else
     echo "Error: no se encontró '$PACKAGES_PATH'"
     exit 1
   fi
-
-  # Leer la lista de acciones
-  if [[ -f "$ENVIRONMENTS_PATH" ]]; then
-    readarray -t entornos < "$ENVIRONMENTS_PATH"
-    for i in "${!entornos[@]}"; do
-      ENVIRONMENTS_PATH[$i]=${entornos[$i]}
-    done
-  else
-    echo "Error: no se encontró '$ENVIRONMENTS_PATH'"
-    exit 1
-  fi
 }
 
-function min() {
-  if [[ ${#PACKAGES_PATH[@]} -lt ${#ENVIRONMENTS_PATH[@]} ]]; then
-    echo ${#PACKAGES_PATH[@]}
-  else
-    echo ${#ENVIRONMENTS_PATH[@]}
-  fi
-}
-function iteration() {
-  local num_elementos=$(min)
-  # Iteramos sobre la lista de paquetees
-  for ((i=0; i<num_elementos; i++))
-  do
-    # Obtenemos el paquete y la acción correspondiente
-    paquete="${PACKAGES_PATH[$i]}"
-    entorno="${ENVIRONMENTS_PATH[$i]}"
-    # Imprimimos el mensaje correspondiente al paquete
-    print_message "$paquete" "$entorno"
-    
+# Función para instalar paquetes en un entorno
+function install_packages() {
+  local environment="$1"
+  source "$ENV_DIR/$environment/bin/activate"
+  for package in "${packages[@]}"; do
+    echo "Instalando el paquete PIP '$package' en el entorno '$environment'..."
+    echo "$environment --> $package" >> "$REPORT_PATH"
+    pip install "$package"
   done
-  
-}
-function print_message() {
-  local paquete="$1"
-  # Mostrar item 1
-  echo "Paquete: '$paquete'"
-  local entorno="$2"
-  # Mostrar item 2
-  echo "Entorno: '$entorno'"
-  # Imprimimos el mensaje
-  echo "Activando el entorno '$entorno' e instalando el paquete PIP '$paquete'."
-  echo "'$entorno' --> '$paquete'" >> "$REPORT_PATH"
-  # Activate environment
-  source "$ENV_DIR/$entorno/bin/activate"
-  sleep 2
-  # Instalar paquete PIP
-  pip install "$paquete"
-  # Deactivate environment
   deactivate
-  sleep 3
 }
-function check_pip() {
-  pip list
-}
-############################################################################################
+
 # Función principal
 function main() {
   verify_list1
   verify_list2
   check_report_file
-  read_lists
-  iteration
+  read_packages
+  for environment in $(cat "$ENVIRONMENTS_PATH"); do
+    install_packages "$environment"
+  done
   check_pip
 }
+
 main
