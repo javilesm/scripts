cb4eb581f46fb5766039297d7e93e9701dbc41cc
JORGE AVILES <68437746+javilesm@users.noreply.github.com>
2023-10-28 08:51:33 -0600

    Update add_antares_workorder.sh

--- NGINX/add_antares_workorder.sh
+++ NGINX/add_antares_workorder.sh
@@ -58,35 +58,66 @@ function insert_record() {
     mysql -u"$db_user" -p"$db_password" -D "$db_name" -e "INSERT INTO $db_workorder_table (T_WORKORDER, DESCRIPTION, T_PRODUCT, REGISTERED_DOMAIN, T_PARTITION, FECHA_INICIO_DE_VIGENCIA, FECHA_FIN_DE_VIGENCIA, WORKORDER_FLAG, ENTRY_STATUS, CREATE_DATE, CREATE_BY, UPDATE_DATE, UPDATE_BY) VALUES ('$current_consecutive', '$description', '$t_product', '$registered_domain', '$t_partition', '$fecha_inicio_vigencia', '$fecha_fin_vigencia', '$workorder_flag', '$entry_status', '$create_date', '$create_by', '$update_date', '$update_by');"
 }
 
-# Función principal para la interfaz de usuario
-function main_dialog() {
-    local t_product="8"
-    local registered_domain=""
-    local t_partition="xvda1"
-    local fecha_inicio_vigencia="2023-01-01 00:00:00"
-    local fecha_fin_vigencia="2024-01-01 00:00:00"
-    local workorder_flag="1"
-    local entry_status="0"
-
+# Función para mostrar el formulario de agregar registro
+function show_add_record_form() {
+    # Mostrar el valor vigente de "description" en el formulario
     dialog --form "Agregar registro a la tabla $db_workorder_table" 20 60 14 \
-        "t_product:" 1 1 "$t_product" 1 12 10 0 \
-        "registered_domain:" 2 1 "$registered_domain" 2 19 30 0 \
-        "t_partition:" 3 1 "$t_partition" 3 14 10 0 \
-        "Fecha inicio de vigencia:" 4 1 "$fecha_inicio_vigencia" 4 25 19 0 \
-        "Fecha fin de vigencia:" 5 1 "$fecha_fin_vigencia" 5 23 19 0 \
-        "workorder_flag:" 6 1 "$workorder_flag" 6 16 10 0 \
-        "entry_status:" 7 1 "$entry_status" 7 15 10 0 2> /tmp/input.txt
+        "t_product:" 1 1 "8" 1 12 10 0 \
+        "registered_domain:" 2 1 "" 2 19 30 0 \
+        "t_partition:" 3 1 "xvda1" 3 14 10 0 \
+        "Fecha inicio de vigencia:" 4 1 "2023-01-01 00:00:00" 4 25 19 0 \
+        "Fecha fin de vigencia:" 5 1 "2024-01-01 00:00:00" 5 23 19 0 \
+        "workorder_flag:" 6 1 "1" 6 16 10 0 \
+        "entry_status:" 7 1 "0" 7 15 10 0 2> /tmp/input.txt
 
     read -r t_product registered_domain t_partition fecha_inicio_vigencia fecha_fin_vigencia workorder_flag entry_status < /tmp/input.txt
 
-    insert_record "$t_product" "$registered_domain" "$t_partition" "$fecha_inicio_vigencia" "$fecha_fin_vigencia" "$workorder_flag" "$entry_status" "$db_user" "$db_user"
-    show_success_message
-    sudo rm /tmp/input.txt
+    # Confirmar inserción
+    dialog --yesno "¿Deseas agregar este registro?" 7 40
+    response=$?
 
-    # Mostrar la tabla después de la inserción
-    dialog --msgbox "Contenido de la tabla $db_workorder_table después de la inserción:" 10 40
-    show_workorder_table
+    if [ $response -eq 0 ]; then
+        insert_record "$t_product" "$registered_domain" "$t_partition" "$fecha_inicio_vigencia" "$fecha_fin_vigencia" "$workorder_flag" "$entry_status" "$db_user" "$db_user"
+        show_success_message
+    fi
+
+    sudo rm /tmp/input.txt
 }
+
+# Función para mostrar la tabla de workorders
+function show_workorder_dialog() {
+    tmpfile=$(mktemp /tmp/workorders.XXXXXXXXXX)
+    show_workorder_table > "$tmpfile"
+    dialog --textbox "$tmpfile" 20 60
+    rm -f "$tmpfile"
+}
+
+
+# Función principal para la interfaz de usuario
+function main_dialog() {
+    while true; do
+        dialog --menu "Menú principal" 15 40 4 \
+            1 "Agregar nuevo registro" \
+            2 "Mostrar tabla $db_workorder_table" \
+            3 "Salir" 2> /tmp/menu_choice.txt
+
+        choice=$(cat /tmp/menu_choice.txt)
+
+        case $choice in
+            1)
+                show_add_record_form
+                ;;
+            2)
+                show_workorder_dialog
+                ;;
+            3)
+                clear  # Limpiar la terminal
+                break
+                ;;
+        esac
+    done
+}
+
 
 # Inicializar el script
 get_last_consecutive
